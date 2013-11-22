abf68bd626c2bc3da2fc7bcd8ea46001fe9e485f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2013-11-22 12:21:35 +0530

    Prose to HoTT expression: basic parsing

---
+++ src/main/scala/NLPHoTT.scala
@@ -0,0 +1,31 @@
+package provingGround
+
+import provingGround.NlpProse._
+import provingGround.HoTT._
+import provingGround.TheoryTypes._ 
+
+
+import scala.language.implicitConversions
+
+/** Parses prose to HoTT
+ *  Gives context-free expressions, not actual typed terms.
+ *  Warning: Should handle conjunctions carefully
+ */
+object NLPHoTT{
+  val parse: ProseTree =>  Term = {
+    case ProseTree(root, List()) => TermSym(root.word)
+    
+    case Cop(first, second) => Is(parse(first), parse(second))
+    
+    case Conj(conj, _, sub, rest) => Conjunct(conj, parse(sub), parse(rest))
+    
+    case Modifier(_, sub, rest) => Apply(parse(sub), parse(rest))
+    
+    case Argument(_, sub, rest) => Apply(parse(rest), parse(sub))
+    
+    case Parataxis(_, sub, rest) => parse(rest) \\ parse(sub)
+    
+    case t => TermSym(t)
+  }
+	
+}
