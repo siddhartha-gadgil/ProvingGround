f7123b43ad846e4ab2eff3014ef211cd5de4b04a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2014-04-07 17:08:50 +0530

    small dart changes

--- web/provingground.dart
+++ web/provingground.dart
@@ -1,6 +1,18 @@
 import 'dart:html';
+import 'package:angular/angular.dart';
+import 'package:js/js.dart';
+
+// Temporary, please follow https://github.com/angular/angular.dart/issues/476
+@MirrorsUsed(override: '*')
+import 'dart:mirrors';
+
+
+var hub = context['MathJax']['Hub'];
 
 void main() {
+  
+  ngBootstrap();
+  
   querySelector("#sample_text_id")
       ..text = "Click me!"
       ..onClick.listen(reverseText);
@@ -12,5 +24,7 @@ void reverseText(MouseEvent event) {
   for (int i = text.length - 1; i >= 0; i--) {
     buffer.write(text[i]);
   }
+  hub;
+//  hub.callMethod("Typeset", []);
   querySelector("#sample_text_id").text = buffer.toString();
 }
