f5ddf3dbc19309cfd14db2a3312d83d4e0b519b7
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2014-06-26 12:28:01 +0530

    wikiread compiles, futures and playframework

--- src/main/scala/WikiRead.scala
+++ src/main/scala/WikiRead.scala
@@ -16,32 +16,50 @@ object WikiRead{
   def getXML(url: String) = WS.url(url).get().map{
     response =>
     	response.xml 
-}
+  }
   
   val baseUrl = "http://en.wikipedia.org"
     
   case class MathPage(title: String, url: String){
 	  val isList = title.containsSlice("List of") && title.containsSlice("topics")
   } 
+  
+  def masterpage = MathPage("Lists of mathematics topics", baseUrl+"/wiki/Lists_of_mathematics_topics")
+  
+  
+  def mathPages(nodes: NodeSeq) = 
+        for(node <- nodes; a <- (node \ "a"); h <- a \ "@href" if !(h.text.startsWith("#"))) yield MathPage(a.text,  h.text) 
+    
+  
+  def nextgen(pgsfut: Future[Seq[MathPage]]) = pgsfut map (_ filter (_.isList)) flatMap ((pgs) => 
+    Future.sequence(
+        pgs map ((pg) => 
+      getXML(pg.url) map ((xml) => mathPages(xml))
+      ))
+        ) map (_.flatten)
+        
+  
+  
+  def saveAll(pgsfut: Future[Seq[MathPage]])(implicit save: MathPage => Unit) = {
+    pgsfut.onSuccess{
+      case pgs => (pgs filter (!_.isList)).foreach(save)
+    }
+  }
+  
+  val topPagesFut = getXML(masterpage.url) map (mathPages(_))
+  
+  val otherPagesFut = nextgen(topPagesFut)
     
   val masterlist = for (nodeseq <- getXML(baseUrl+"/wiki/Lists_of_mathematics_topics")) yield (nodeseq \\ "li")
   
-  def mathPages(nodes: NodeSeq) = {
-        for(node <- nodes; a <- (node \ "a"); h <- a \ "@href" if !(h.text.startsWith("#"))) yield MathPage(a.text,  h.text) 
-    } 
-  
-  val toppagesFut = masterlist map(mathPages(_))
-  
-  val listListsFut = toppagesFut map (_ filter (_.isList)) /*flatMap {(pgs) => 
-    Future.sequence(pgs map ((pg) => getXML(pg.url)))} */
-  
-  def savepages(save: NodeSeq => Unit) = toppagesFut onSuccess{case (pgs: Seq[MathPage]) =>
-    for (pg <- pgs) yield (getXML(pg.url).onSuccess{case xml => save(xml)})
-    
-    listListsFut.onSuccess{
-      case pgs => pgs map {(pg)=>
-        getXML(pg.url).onSuccess{case xml => save(xml)}
-        }
-    }
+   def savePages(implicit save: MathPage => Unit) = {
+    saveAll(topPagesFut)
+    saveAll(otherPagesFut)
   }
+  
+  object test{
+    implicit val save: MathPage => Unit = println(_)
+  }
+  
+  
 }
