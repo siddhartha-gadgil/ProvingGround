563febf8b66a52a0f4339e333415bbc825de9928
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2014-08-27 12:17:36 +0530

    Copied Andrews-Curtis types to AndrewsCurtisModel and stopped importing. DynDSt now created without infinite loops.

--- app/models/AndrewsCurtisModel.scala
+++ app/models/AndrewsCurtisModel.scala
@@ -1,5 +1,5 @@
 package provingground
-import provingground.AndrewsCurtis.{ACMoveType, allMoves, MoveTypeList, Move}
+//import provingground.AndrewsCurtis.{ACMoveType, allMoves, MoveTypeList, Move}
 import provingground.Collections._
 import provingground.FreeGroups._
 import provingground.DynInterface._
@@ -9,7 +9,12 @@ import scala.language.implicitConversions
 
 import play.api.data._
 import play.api.data.Forms._
+
+import play.api.libs.json._
+
 import math._
+
+
 
 //import play.api.libs.iteratee._
 
@@ -19,8 +24,116 @@ import math._
 import math.pow
 
 object AndrewsCurtisModel{
+    /*
+   * The type of moves
+   */
+  type MoveType = ACMoveType
+  
+  type Vert = Presentation
+  
+  /*
+   * Andrews-Curtis moves
+   */
+  trait ACMoveType 
+  
+  case object ACStabMv extends ACMoveType
+  case object ACDeStabMv extends ACMoveType
+  case object RtMultMv extends ACMoveType
+  case object LftMultMv extends ACMoveType
+  case object ConjMv extends ACMoveType
+  case object InvMv extends ACMoveType
+  
+ 
+  object ACMoveType{
+    def fromString(mv : String) : ACMoveType = (MoveTypeList find (_.toString == mv)).get
+  }
+  
+  val MoveTypeList : List[ACMoveType] = List(ACStabMv, ACDeStabMv, RtMultMv, LftMultMv, ConjMv, InvMv)
     
+  implicit object ACMoveFreqFormat extends Format[FiniteDistribution[ACMoveType]]{
+    def reads(js : JsValue) : JsResult[FiniteDistribution[ACMoveType]] = {
+      val pmf = for ((x, y) <- js.as[Map[String, Double]]) yield Weighted(ACMoveType.fromString(x), y)
+      JsSuccess(FiniteDistribution(pmf.toSeq))
+    }
     
+    def writes(fd : FiniteDistribution[ACMoveType]): JsValue = {
+      Json.toJson((for (Weighted(x, y) <- fd.pmf) yield (x.toString, y)).toMap)
+    } 
+  }
+ 
+    trait Move extends (Vert => Vert){
+    val mvType : MoveType
+  }
+  
+  case object ACStab extends Move{
+    def apply(p: Presentation) = p.ACstab
+    
+    val mvType = ACStabMv
+  }
+  
+  case object ACDeStab extends Move{
+    def apply(p: Presentation) = p.ACdestab
+    
+    val mvType = ACStabMv
+  }
+  
+  case class RtMult(k : Int, l : Int) extends Move{
+    def apply(p: Presentation) = p.rtmult(k, l)
+    
+    val mvType = ACStabMv
+  }
+  
+  case class LftMult(k : Int, l : Int) extends Move{
+    val mvType = ACStabMv
+    
+    def apply(p: Presentation) = p.lftmult(k, l)
+  }
+  
+  case class Conj(k : Int, l : Int) extends Move{
+    def apply(p: Presentation) = p.conj(k, l)
+    
+    val mvType = ACStabMv
+  }
+  
+  case class Inv(k : Int) extends Move{
+    def apply(p: Presentation) = p.inv(k)
+    
+    val mvType = ACStabMv
+  }
+  
+  
+  
+  
+  /*
+   * Multiplicity of moves of a given type given a presentation. In general this should be a function of the presentation.
+   */
+  def multiplicity(rk: Int) : MoveType => Long = {
+    case ACStabMv => 1 : Long
+    case ACDeStabMv => (1 : Long) // can be zero in practice
+    case RtMultMv => (rk : Long) * ((rk : Long) - 1)
+    case LftMultMv => (rk : Long) * ((rk : Long) - 1)
+    case ConjMv => (rk : Long) * ((rk : Long) * 2 - 1)
+    case InvMv => (rk : Long)
+  }
+  
+  def allMoves(pres : Presentation): MoveType => List[Move] = {
+    case ACStabMv => List(ACStab)
+    case ACDeStabMv => 
+      if (pres.ACstabilized) List(ACDeStab) else List()
+    case RtMultMv => 
+      val n = pres.rank
+      (for (j <-0 to n-1; k <- 0 to n-1 if j != k) yield RtMult(j, k)).toList
+    case LftMultMv => 
+      val n = pres.rank
+      (for (j <-0 to n-1; k <- 0 to n-1 if j != k) yield LftMult(j, k)).toList
+    case ConjMv => 
+      val n = pres.rank
+      (for (j <-0 to n-1; k <- -n to n if k != 0) yield Conj(j, k)).toList
+    case InvMv => 
+      val n = pres.rank
+      (for (j <-0 to n-1) yield Inv(j)).toList
+  }
+  
     val moves = (pres: Presentation, mvtyp : ACMoveType) => allMoves(pres)(mvtyp).toSet map ((mv : Move) => mv(pres))
     
     val learner = new MoveLearner(MoveTypeList, moves)
---
+++ app/worksheets/ACrun.sc
@@ -0,0 +1,41 @@
+package worksheets
+
+import provingground.Collections._
+import provingground.FreeGroups.nullpres
+import provingground.MoveLearner._
+import provingground.MoveLearner
+import provingground.AndrewsCurtisModel._
+
+object ACrun {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+//  val presGen = PresentationGen()
+  
+  val np = nullpres                               //> np  : provingground.FreeGroups.Presentation = <; >
+  
+  val presdest = FiniteDistribution(Seq(Weighted(np, 1.0)))
+                                                  //> presdest  : provingground.Collections.FiniteDistribution[provingground.FreeG
+                                                  //| roups.Presentation] = [<; > : 1.0]
+  
+  val mt = MoveTypeList                           //> mt  : List[provingground.AndrewsCurtisModel.ACMoveType] = List(ACStabMv, ACD
+                                                  //| eStabMv, RtMultMv, LftMultMv, ConjMv, InvMv)
+  
+  val mvs = moves                                 //> mvs  : (provingground.FreeGroups.Presentation, provingground.AndrewsCurtisMo
+                                                  //| del.ACMoveType) => scala.collection.immutable.Set[provingground.AndrewsCurti
+                                                  //| sModel.Vert] = <function2>
+  
+  
+  val l= new MoveLearner(MoveTypeList, moves)     //> l  : provingground.MoveLearner[provingground.AndrewsCurtisModel.Vert,proving
+                                                  //| ground.AndrewsCurtisModel.ACMoveType] = provingground.MoveLearner@4c4f7cf7
+    
+    
+  
+  val ll = learner                                //> ll  : provingground.MoveLearner[provingground.AndrewsCurtisModel.Vert,provin
+                                                  //| gground.AndrewsCurtisModel.ACMoveType] = provingground.MoveLearner@76956344
+  
+  
+  val dst = l.DynDst(FiniteDistribution(Seq(Weighted(nullpres, 1.0))), unifmoves, 0.7).norm
+                                                  //> dst  : Double = 2.7
+
+ 
+}
