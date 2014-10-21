3fb40cee1eb20912eeff57984abf7267a63ff548
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2014-10-21 14:15:03 +0530

    Maybe Type

---
+++ app/provingground/functionfinder/MaybeTyp.scala
@@ -0,0 +1,26 @@
+package provingground.functionfinder
+
+import provingground.HoTT._
+import ScalaRep._
+import scala.reflect.runtime.universe.{Try => UnivTry, Function => FunctionUniv, _}
+
+object MaybeTyp {
+  case class MaybeTyp[U<: Term](tp: Typ[U]) extends SmallTyp
+  
+  case class Maybe[U <: Term](opt: Option[U], tp: Typ[U]) extends AtomicTerm{
+    val typ = MaybeTyp(tp)
+  }
+  
+  def get[U <: Term] : PartialFunction[Maybe[U], U] = {
+    case Maybe(Some(x), _) => x
+  }
+  
+  case class MaybeRep[U <: Term : TypeTag](typ: Typ[U]) extends ScalaRep[Term, U]{
+    def apply (term: U) = Maybe(Some(term), typ)
+    
+    def unapply(term: Term) = term match {
+      case Maybe(Some(x : U), `typ`) => Some(x)
+      case _ => None
+    }
+  }
+}
