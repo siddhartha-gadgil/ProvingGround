e0fb33eb0c0122de6de87c68f7a8b204681df0bd
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2015-05-21 16:09:39 +0530

    filelog corrected; debreeze

--- digressions/src/main/scala/compact_enumeration/SpecialFunctions.scala
+++ digressions/src/main/scala/compact_enumeration/SpecialFunctions.scala
@@ -1,7 +1,7 @@
 package compact_enumeration
 
-import breeze.math._
-import breeze.numerics
+import spire.math._
+
 import scala.language.implicitConversions
 
 
@@ -11,93 +11,93 @@ import scala.language.implicitConversions
 object SpecialFunctions {
   trait SpecialFunction extends (Double => Double){
     def +(that : Fn) = Add(this, that)
-    
+
     def *(that : Fn) = Mult(this, that)
-    
+
     def circ(that : Fn) = Compose(this, that)
-    
+
     def apply(that: Fn) = Compose(this, that)
-    
+
     def /(that: Fn) = Ratio(this, that)
-    
+
     def -(that : Fn) = this + (-1.0 * that)
-    
+
     def unary_- = 0.0 - (1.0 * this)
   }
-  
-  
+
+
   private type Fn = SpecialFunction
-  
+
   case class Add(first: Fn, second: Fn) extends Fn{
     def apply(x: Double) = first(x) + second(x)
   }
-  
+
   case class Mult(first: Fn, second: Fn) extends Fn{
     def apply(x: Double) = first(x) * second(x)
   }
-  
+
   object Ratio{
     def apply(first: Fn, second: Fn) = first *  Reciprocal(second)
-    
+
     def unapply(fn: Fn) : Option[(Fn, Fn)] = fn match {
       case Mult(num, Reciprocal(den)) => Some((num, den))
       case _ => None
     }
   }
-  
-  
+
+
   case class Reciprocal(denominator : Fn) extends Fn{
     def apply(x: Double) = 1.0 / x
   }
-  
+
   case class Compose(first: Fn, second: Fn) extends Fn{
     def apply(x: Double) = first(second(x))
     override def toString = s"${first} \u2218 ${second}"
   }
-  
+
   case class Func(defn: Double => Double) extends SpecialFunction{
     def apply(x: Double) = defn(x)
   }
-  
+
   case class Const(value: Double) extends SpecialFunction{
     def apply(x: Double) = value
   }
-  
+
   implicit def const(value: Double): SpecialFunction = Const(value)
-  
+
   object Const{
     def derivative: PartialFunction[SpecialFunction, SpecialFunction] = {
       case Const(_) => Const(0)
     }
   }
-  
 
-  
-  val sin = Func((x: Double) => numerics.sin(x))
-  
-  val cos = Func((x: Double) => numerics.cos(x))
-  
-  val log = Func((x: Double) => numerics.log(x))
-  
-  val exp = Func((x: Double) => numerics.exp(x))
-  
+
+
+  val sin = Func((x: Double) => math.sin(x))
+
+  val cos = Func((x: Double) => math.cos(x))
+
+  val log = Func((x: Double) => math.log(x))
+
+  val exp = Func((x: Double) => math.exp(x))
+
   case class Pow(a: Double) extends Fn{
-    def apply(x: Double) = numerics.pow(x, a)
+    def apply(x: Double) = math.pow(x, a)
   }
-  
+
   object Pow{
     def derivative: PartialFunction[SpecialFunction, SpecialFunction] = {
       case Pow(a) => a * Pow(a -1)
     }
   }
-  
+
   val id = Pow(1)
 
   val sq = Pow(2)
-  
+
   val sqrt = Pow(1.0/ 2)
-  
-  def derivativeTable = 
+
+  def derivativeTable =
     Const.derivative orElse
     Pow.derivative orElse
     Map(sin -> cos, cos ->  -sin, exp -> exp, log -> 1 / id)
--- jvm/app/provingground/QDI.scala
+++ jvm/app/provingground/QDI.scala
@@ -16,48 +16,48 @@ import scala.annotation._
  */
 object QDI {
   lazy val desktop = Desktop.getDesktop
-  
+
   def writeFile(text: String, fileName: String, append: Boolean = false) ={
-    val writer = new FileWriter(fileName, append)    
+    val writer = new FileWriter(fileName, append)
     writer.write(text)
     writer.close
   }
-  
+
   def viewPage(body: Node, fileName: String = "qdi.html") ={
     val page = <html>{head}<body>{body}</body></html>
     writeFile(page.toString, fileName)
     val file = new File(fileName)
-    desktop.browse(file.toURI)  
+    desktop.browse(file.toURI)
   }
-  
+
   def view(ps : Node*) = {
     val fileName="qdi.html"
     val page = <html>{head}<body>{NodeSeq.fromSeq(ps)}</body></html>
     writeFile(page.toString, fileName)
     val file = new File(fileName)
-    desktop.browse(file.toURI) 
+    desktop.browse(file.toURI)
   }
-     
-  
+
+
   def p(s: String) : Node = <p> {s} </p>
-  
+
   trait WebView[A]{
     def asXML(a: A): Node
   }
-  
+
   implicit def toXML[A: WebView](a: A) : Node = {
     implicitly[WebView[A]].asXML(a)
   }
-  
+
   implicit def listView[A: WebView] : WebView[List[A]] = new WebView[List[A]]{
-    def asXML(as: List[A]) = 
+    def asXML(as: List[A]) =
       {val l = as map ((a) => implicitly[WebView[A]].asXML(a))
-      <div>{NodeSeq.fromSeq(l)}</div>      
+      <div>{NodeSeq.fromSeq(l)}</div>
       }
   }
-  
+
   import Collections._
-  
+
   implicit def fdDiv[A](fd: FiniteDistribution[A]) : Node = {
     val lst = fd.pmf.toList.sortBy((x) => -x.weight).zipWithIndex
     val title = <div class="atom">
@@ -75,10 +75,10 @@ object QDI {
 				</div>)
    <div class="finite-distribution"> {NodeSeq.fromSeq(title +: nodeList)} </div>
   }
-  
+
   implicit def fdListDiv[A](fds: List[FiniteDistribution[A]]) : Node = {
     val lst = fds.last.pmf.toList.sortBy((x) => -x.weight).zipWithIndex
-    def entropies(a: A) = for (fd <- fds) 
+    def entropies(a: A) = for (fd <- fds)
       yield (<span class="entropy">{-math.log(fd(a))/math.log(2)}</span>)
     val nodeList = for ((Weighted(a, x), j) <- lst)
       yield (<div class="atom-list">
@@ -88,72 +88,72 @@ object QDI {
         </div>)
    <div class="finite-distribution"> {NodeSeq.fromSeq(nodeList)} </div>
   }
-  
+
   implicit def fdString[A](fd: FiniteDistribution[A]) = {
     val l = for (Weighted(x, p) <- fd.pmf) yield s"""("$x",$p)"""
     l.mkString(";")
   }
-  
+
   def row(xs: List[String]) = {
-    def cls(i: Int) = if (i % 2 == 0) "even" else "odd" 
+    def cls(i: Int) = if (i % 2 == 0) "even" else "odd"
     val spans = {
       for ((x, i) <- xs.zipWithIndex) yield <span class={cls(i)}>{x}</span>
     }
     <div class="row">{NodeSeq.fromSeq(spans)}</div>
   }
-  
+
   implicit def tableDiv(xy: List[List[Any]]) = {
     val rows = xy map ((r) => row(r map (_.toString)))
     <div class="table">{NodeSeq.fromSeq(rows)}</div>
   }
-  
+
   implicit def tableString(xy: List[List[Any]]) = {
     val rows = xy map ((r) => (r map ((x) => s""""$x"""")).mkString(","))
     rows.mkString(";")
   }
-  
+
   trait Logger{
     def log(x: String): Unit
-    
+
     var closed: Boolean = false
-    
+
     def get: Option[Iterator[String]]
-    
+
     def close: Unit
   }
-  
+
   class MemLog extends Logger{
     var mem : Vector[String] = Vector()
-    
+
     def log(x: String) = (mem = mem :+ x)
-    
+
     def get = if (closed) Some(mem.toIterator) else None
-    
+
     def close = {closed = true}
   }
-  
+
   class FileLog(filename: String, append: Boolean = false) extends Logger{
     val fl = new FileWriter(filename, append)
-    
+
     val writer = new BufferedWriter(fl)
 
-    def log(x: String) = writer.write(x); writer.newLine
-    
-    def close = closed = true; fl.close
-    
+    def log(x: String) = {writer.write(x); writer.newLine}
+
+    def close = {closed = true; writer.close}
+
     def get = if (closed) Some(readFile(filename)) else None
   }
-  
+
   def readFile(filename: String) = scala.io.Source.fromFile(filename).getLines
-  
+
   @tailrec def iterLog[A](init: A, dyn: A => A, steps: Int, logger: Logger) : A ={
     logger.log(init.toString)
     if (steps <1) init
-    else iterLog(dyn(init), dyn, steps, logger) 
+    else iterLog(dyn(init), dyn, steps, logger)
   }
-  
+
   def asyncIterLog[A](init: A, dyn: A => A, steps: Int, logger: Logger) = Future(iterLog[A](init: A, dyn: A => A, steps: Int, logger: Logger))
-  
+
   val css = """
     .index {
         color: black;
@@ -187,12 +187,12 @@ object QDI {
       width: 200px;
       }
     """
-  
-  val head= 
+
+  val head=
     <head>
 			<style type="text/css">
       {css}
     	</style>
    </head>
-    
+
 }
