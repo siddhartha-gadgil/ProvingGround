88447dbedb12401482ddd1ade15f502943183a45
Sayantan Khan <bolt.khan@gmail.com>
2015-06-19 19:44:16 +0530

    Added a move generator which generates functions from presentation to presentation.

---
+++ andrewscurtis/src/main/scala/provingground/MoveGenerator.scala
@@ -0,0 +1,30 @@
+package provingground.andrewscurtis
+
+import provingground.andrewscurtis.FreeGroups._
+
+object MoveGenerator {
+  
+  def genAllInv(sz: Int): List[Presentation => Presentation] = {
+    (0 until sz).toList map ((x:Int) => ((pres: Presentation) => Presentation.inv(pres, x)))
+  }
+
+  def genLftMult(sz: Int): List[Presentation => Presentation] = {
+    val f = ((k: Int) => ((0 until sz).toList map ((l: Int) => ((pres: Presentation) => Presentation.lftmult(pres, k, l)))))
+    (0 until sz).toList flatMap f
+  }
+
+  def genRtMult(sz: Int): List[Presentation => Presentation] = {
+    val f = ((k: Int) => ((0 until sz).toList map ((l: Int) => ((pres: Presentation) => Presentation.rtmult(pres, k, l)))))
+    (0 until sz).toList flatMap f
+  }
+
+  def genConjRelators(sz: Int): List[Presentation => Presentation] = {
+    val f = ((k: Int) => ((0 until sz).toList map ((l: Int) => ((pres: Presentation) => Presentation.conjRelators(pres, k, l)))))
+    (0 until sz).toList flatMap f
+  }
+
+  def genAllMoves(rank: Int, sz: Int): List[Presentation => Presentation] = {
+    val takesz = List(genAllInv(_), genLftMult(_), genRtMult(_), genConjRelators(_))
+    takesz flatMap ((f: Int => List[Presentation => Presentation]) => f(sz))
+  }
+}
