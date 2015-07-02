3df18f058d1671dea22f55eb4709deedbdb70807
Sayantan Khan <bolt.khan@gmail.com>
2015-07-02 20:40:58 +0530

    Used type aliases for improved readability.

--- andrewscurtis/src/main/scala/provingground/DiffStructure.scala
+++ andrewscurtis/src/main/scala/provingground/DiffStructure.scala
@@ -7,39 +7,47 @@ import provingground.DiffbleFunction._
 import provingground.andrewscurtis.Moves._
 
 object DiffStructure {
+  /*
+   * Type aliases
+   */
 
-  def genMoveFn(mf: AtomicMove) = moveFn(mf)
+  type FD[X] = FiniteDistribution[X]
+  type M = AtomicMove
+  type V = Moves
+  type P = Presentation
 
-  def genWtDyn(mf: AtomicMove) = {
-    val wtdyn = weightedDyn[AtomicMove, FiniteDistribution[Moves]]
+  def genMoveFn(mf: M) = moveFn(mf)
+
+  def genWtDyn(mf: M) = {
+    val wtdyn = weightedDyn[M, FD[V]]
     wtdyn(mf, genMoveFn(mf))
   }
 
-  def genCollection(lst: List[AtomicMove]) = lst map genWtDyn
+  def genCollection(lst: List[M]) = lst map genWtDyn
 
-  def addCollection(lst: List[AtomicMove]) = vbigsum(genCollection(lst))
+  def addCollection(lst: List[M]) = vbigsum(genCollection(lst))
 
-  def genExtendM(lst: List[AtomicMove]) = extendM(addCollection(lst))
+  def genExtendM(lst: List[M]) = extendM(addCollection(lst))
 
-  def iterateDiff(lst: List[AtomicMove], iterations: Int = 5) = iterate(genExtendM(lst))(iterations)
+  def iterateDiff(lst: List[M], iterations: Int = 5) = iterate(genExtendM(lst))(iterations)
 
-  def genPresentationMoveFn(rank: Int) = moveFn(Moves.actOnTriv(rank)(_: Moves))
+  def genPresentationMoveFn(rank: Int) = moveFn(Moves.actOnTriv(rank)(_: V))
 
-  def genProjectionMap(rank: Int, iterations: Int = 5)(lst: List[AtomicMove]) = {
+  def genProjectionMap(rank: Int, iterations: Int = 5)(lst: List[M]) = {
     iterateDiff(lst, iterations) andthen projectV andthen genPresentationMoveFn(rank)
   }
 
-  def getFeedback(presCntn: Double, wrdCntn: Double, scale: Double = 1)(dist: FiniteDistribution[Presentation]) = {
-    val baseweight = presentationWeight(_: Presentation, presCntn, wrdCntn)
+  def getFeedback(presCntn: Double, wrdCntn: Double, scale: Double = 1)(dist: FD[P]) = {
+    val baseweight = presentationWeight(_: P, presCntn, wrdCntn)
     dist.feedback(baseweight) * scale
   }
 
-  def conjugateByFeedback(presCntn: Double, wrdCntn: Double, scale: Double = 1)(rank: Int, iterations: Int = 5)(lst: List[AtomicMove]) = {
+  def conjugateByFeedback(presCntn: Double, wrdCntn: Double, scale: Double = 1)(rank: Int, iterations: Int = 5)(lst: List[M]) = {
     val projectionMap = genProjectionMap(rank, iterations)(lst)
-    projectionMap.^:(getFeedback(presCntn, wrdCntn, scale)(_: FiniteDistribution[Presentation]))
+    projectionMap.^:(getFeedback(presCntn, wrdCntn, scale)(_: FD[P]))
   }
 
-  def genDynamics(conjFunc: ((FiniteDistribution[AtomicMove], FiniteDistribution[Moves])) => (FiniteDistribution[AtomicMove], FiniteDistribution[Moves]))(orig: (FiniteDistribution[AtomicMove], FiniteDistribution[Moves])) = {
+  def genDynamics(conjFunc: ((FD[M], FD[V])) => (FD[M], FD[V]))(orig: (FD[M], FD[V])) = {
     val sumRes = conjFunc(orig)
     (orig._1 ++ sumRes._1, orig._2 ++ sumRes._2)
   }
