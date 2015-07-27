84ccf726b0a81e54aea64785866924d86aed2fd1
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2015-07-27 20:28:34 +0530

    corrected merge

--- core/src/main/scala/provingground/ConstructorPattern.scala
+++ core/src/main/scala/provingground/ConstructorPattern.scala
@@ -130,7 +130,7 @@ import RecFunction._
     val univLevel : Int
 
   }
-  
+
 
 
 
@@ -397,10 +397,10 @@ import RecFunction._
   /**
    * Dependent extension by a constant type  of a constructor-pattern depending on elements of that type.
    */
-  case class CnstDepFuncPtn[U <: Term with Subs[U], V <: Term with Subs[V], C <: Term with Subs[C]](tail: Typ[Term],
-      headfibre : Term => (ConstructorPattern[C, U]{
-      type RecDataType = V}), headlevel: Int = 0)/*(
-      implicit su: ScalaUniv[U])*/ extends
+  case class CnstDepFuncPtn[U <: Term with Subs[U],
+    V <: Term with Subs[V],
+    C <: Term with Subs[C]](tail: Typ[Term],
+      headfibre : Term => (ConstructorPattern[C, U]{type RecDataType = V}), headlevel: Int = 0) extends
       RecursiveConstructorPattern[C, Term, U, FuncLike[Term, U]]{self =>
 
 //    type ArgType = Term
@@ -496,120 +496,4 @@ import RecFunction._
   }
 
 
-  case class RecFn[C<: Term with Subs[C]](W: Typ[Term], X: Typ[C]) extends AnySym
   
-  /**
-   * rec(W)(X) is the value, defined recursively.
-   * @tparam C codomain (scala) type
-   * @tparam F full type of rec
-   */
-  trait RecFunction[C<: Term with Subs[C]]{self =>
-    /**
-     * W in rec(W)(X)
-     */
-    val W: Typ[Term]
-
-    /**
-     * X in rec(W)(X)
-     */
- //   val X : Typ[C]
-
-
-    /**
-     * (scala) type of rec(W)(X)
-     */
-    type FullType <: Term with Subs[FullType]
-
-    def fullTyp(x : Typ[C]): Typ[FullType]
-    
-    /**
-     * induced change to function of the type of rec(W)(X) given change on function W->X;
-     * @param transform function W -> X by which we change functions W -> X, trying the case first.
-     * @return induced changed function.
-     */
-    def pullback(X: Typ[C])(transform: Func[Term, C] => Func[Term, C]): FullType => FullType
-
-    /**
-     * given value for rec(W)(X) corresponding to earlier patterns, returns one including the new case.
-     */
-    def recursion(X: Typ[C])(f: => FullType): FullType
-
-    /**
-     * prepend a constructor
-     */
-     def prepend[U <: Term with Subs[U]](cons: Constructor[C, U]) = {
-      val recdom = (x: Typ[C]) => cons.pattern.recDom(cons.W, x)
-      type D = cons.pattern.RecDataType
-      val caseFn : D => Func[Term, C] => Func[Term, C] = (d) => (f) => cons.pattern.recModify(cons.cons)(d)(f)
-      RecFunctionCons[D, C](recdom, caseFn, this)
-    }
-    
-    def fn(x: Typ[C]) : FullType = recursion(x)(fn(x))      
-      
-  }
-
-  def recFunction[C <: Term with Subs[C], U <: Term with Subs[U]](conss: List[Constructor[C, U]], W: Typ[Term]) = {
-    val init : RecFunction[C] = RecTail[C](W)
-    (init /: conss)(_ prepend _)
-  }
-
-  def recFn[C <: Term with Subs[C], U <: Term with Subs[U]](conss: List[Constructor[C, U]], W: Typ[Term], X: Typ[C]) = 
-    recFunction(conss, W).fn(X)
-  
-/*
-  case class RecProxy[C <: Term](W: Typ[Term], X : Typ[C]) extends AnySym{
-    override def toString = s"rec($W)($X)"
-  }
- */
-
-  /**
-   * container for rec(W)(X) in the case of no constructors.
-   * rec(W)(X) is defined to be formal application of itself.
-   * Lazy lambda is used to avoid infinite loops
-   */
-  case class RecTail[C <: Term with Subs[C]](W: Typ[Term]) extends RecFunction[C]{
-    type FullType = Func[Term, C]
-    
-    def fullTyp(x: Typ[C]) : Typ[FullType] = W ->: x
-
-    private lazy val a = W.Var
-
-    def recursion(X: Typ[C])(f: => FullType) = new LazyLambdaFixed(a, X.symbObj(ApplnSym(f, a)))
-
-    def pullback(X: Typ[C])(transform: Func[Term, C] => Func[Term, C]) = (g : Func[Term, C]) => g
-  }
-
-  /**
-   * cons for recursion function, i.e., adding a new constructor
-   * @param dom domain
-   * @param caseFn given (previous?) rec(W)(X) and function in domain (to be applied to value) matches pattern
-   * @param tail previously added constructors
-   */
-  case class RecFunctionCons[D<: Term with Subs[D], C <: Term with Subs[C]](
-      recdom: Typ[C] => Typ[D],
-      caseFn : D => Func[Term, C] => Func[Term, C],
-      tail: RecFunction[C]) extends RecFunction[C]{
-    val W = tail.W
-
-  //  val X = tail.X
-
-    type FullType = Func[D, tail.FullType]
-    
-    def fullTyp(x: Typ[C]) : Typ[FullType] = FuncTyp(recdom(x), tail.fullTyp(x))
-
-
-    def pullback(X: Typ[C])(transform: Func[Term, C] => Func[Term, C]) = (g) =>
-      {
-       val a = recdom(X).Var
-      new LazyLambdaFixed(a, tail.pullback(X)(transform)(g(a)))
-      }
-
-    def recursion(X: Typ[C])(f: => FullType) ={
-      val a = recdom(X).Var
-      def fn(x: D) = tail.pullback(X)(caseFn(x))(tail.recursion(X)(f(x)))
-      lmbda(a)(fn(a))
-    }
-  }
-
-
-}
