8c64f8a739b33cf15435ba015de20abfedfd8e7c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2015-08-25 21:19:07 +0530

    config changes no effect

--- mantle/src/main/scala/provingground/Hub.scala
+++ mantle/src/main/scala/provingground/Hub.scala
@@ -17,17 +17,24 @@ object Hub{
     withValue("akka.loglevel", ConfigValueFactory.fromAnyRef("INFO")).
      withValue("logger.reactivemongo", ConfigValueFactory.fromAnyRef("INFO")).
      withValue("mongo-async-driver.akka.loglevel", ConfigValueFactory.fromAnyRef("INFO")).
+     withValue("mongo-async-driver.loglevel", ConfigValueFactory.fromAnyRef("INFO")).
     withValue("reactivemongo-akka.loglevel", ConfigValueFactory.fromAnyRef("INFO")).
+     withValue("mongo-async-driver.akka.stdout-loglevel", ConfigValueFactory.fromAnyRef("INFO")).
+         withValue("mongo-async-driver.stdout-loglevel", ConfigValueFactory.fromAnyRef("INFO")).
     withValue("reactivemogo-akka.actor.loglevel", ConfigValueFactory.fromAnyRef("INFO"))
 
-  println(cnf.getString("akka.loglevel"))
+  println(cnf.getConfig("mongo-async-driver").getConfig("akka").getString("loglevel"))
+    println(cnf.getConfig("mongo-async-driver").getString("loglevel"))
+    println(cnf.getConfig("mongo-async-driver").getConfig("akka").getString("stdout-loglevel"))
+    println(cnf.getConfig("mongo-async-driver").getString("stdout-loglevel"))
+  
 
-  val driver = new MongoDriver(Some(cnf))
-  val connection = driver.connection(List("localhost"))
+  lazy val driver = new MongoDriver(Some(cnf))
+  lazy val connection = driver.connection(List("localhost"))
 
   // Gets a reference to the database "plugin"
-  implicit val db : DefaultDB = connection("proving-ground")
+  implicit lazy val db : DefaultDB = connection("proving-ground")
 
-  val system = ActorSystem("proving-ground")
+  lazy val system = ActorSystem("proving-ground")
 
 }
