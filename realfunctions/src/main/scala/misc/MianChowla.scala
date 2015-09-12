a8036e1fbf9d06429f47131785074f4d42d36ca1
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2015-09-12 09:00:47 +0530

    Mian-Chowla

---
+++ realfunctions/src/main/scala/misc/MianChowla.scala
@@ -0,0 +1,30 @@
+package misc
+
+import spire.algebra._
+import spire.math._
+import spire.implicits._
+
+object MianChowla{
+@annotation.tailrec
+  def firstGap(set : Set[SafeLong], accum: SafeLong = 1) : SafeLong = {
+    if (set contains(accum)) firstGap(set, accum + 1) else accum
+  }
+
+  def xyz(as: Traversable[SafeLong]) : Set[SafeLong] = {
+    val set = as.toSet
+    for (x <- set; y <- set; z <- set) yield (x + y - z)
+  }
+
+  def nextNum(as: Traversable[SafeLong], n: SafeLong) = firstGap(xyz(as), n)
+
+  val seq : Stream[SafeLong] = Stream.from(1) map ((n: Int) =>
+    if (n ==1) SafeLong(1) else nextNum(seq take (n -1), n: SafeLong)
+  )
+
+  def view(n: Int) = seq.take(n).toList
+
+  def print(n: Int) = {
+    val iter = seq.zipWithIndex.take(n).toIterator
+    for ((x, y) <- iter) println((x, y+1, log(x.toDouble)/log(y.toDouble + 1)))
+  }
+}
