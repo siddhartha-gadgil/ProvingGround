64f0aaf5ae2abd2fdbda1c5eeb3dea0bf728c6bd
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2016-01-21 21:38:41 +0530

    more loops in batch

--- mantle/src/main/scala/provingground/andrewscurtis/batch/Dec_15_2015.scala
+++ mantle/src/main/scala/provingground/andrewscurtis/batch/Dec_15_2015.scala
@@ -5,11 +5,11 @@ import provingground._
 import andrewscurtis._
 
 object Dec_15_2015 {
-  val mickey = StartData("Mickey")
-  
-  val donald = StartData("Donald", wrdCntn = 0.05, steps = 4)
-  
-  val goofy = StartData("Goofy", steps = 6, strictness = 2.5)
-  
+  val mickey = StartData("Mickey", steps = 4)
+
+  val donald = StartData("Donald", wrdCntn = 0.05, steps = 6)
+
+  val goofy = StartData("Goofy", steps = 8, strictness = 2.5)
+
   val pluto = StartData("Pluto", steps = 8, wrdCntn = 0.03)
 }
