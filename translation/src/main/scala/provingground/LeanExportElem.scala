91b64e38d64e45da821f34d0948ac8d0a87f0717
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2016-03-08 15:18:28 +0530

    definitions extracted from lean export format

--- translation/src/main/scala/provingground/LeanExportElem.scala
+++ translation/src/main/scala/provingground/LeanExportElem.scala
@@ -5,16 +5,18 @@ import scala.util.Try
 sealed trait LeanExportElem
 
 object LeanExportElem {
-  case class Data(index: Int, tpe: String, params: List[String])
+  case class Data(index: Long, tpe: String, params: List[String])
 
   object Data {
-    def find(ds: List[Data], index: Int, tpes: String*) =
+    def find(ds: Vector[Data], index: Long, tpes: String*) =
       ds find ((d) => d.index == index && tpes.toSet.contains(d.tpe))
 
-    def get(s: String) : Option[Data] = Try{
+    def read(s: String) : Option[Data] = Try{
       val head :: tpe :: params = s.split(" ").toList
-      Data(head.toInt, tpe, params)
+      Data(head.toLong, tpe, params)
     }.toOption
+
+    def readAll(lines: Vector[String]) = (lines map (read)).flatten
   }
 
   sealed trait Name extends LeanExportElem
@@ -24,15 +26,15 @@ object LeanExportElem {
 
     case class NameString(env: Name, name: String) extends Name
 
-    case class NameInt(env: Name, number: Int) extends Name
+    case class NameLong(env: Name, number: Long) extends Name
 
-    def get(ds: List[Data], index: Int): Option[Name] =
+    def get(ds: Vector[Data], index: Long): Option[Name] =
       if (index == 0) Some(anonymous)
       else Data.find(ds, index, "#NS", "#NI") flatMap {
         case Data(_, "#NS", List(nid, name)) =>
-          get(ds, nid.toInt) map (NameString(_, name))
+          get(ds, nid.toLong) map (NameString(_, name))
         case Data(_, "#NI", List(nid, id)) =>
-          get(ds, nid.toInt) map (NameInt(_, id.toInt))
+          get(ds, nid.toLong) map (NameLong(_, id.toLong))
       }
   }
 
@@ -49,15 +51,15 @@ object LeanExportElem {
 
     case class Global(name: Name) extends Univ
 
-    def get(ds: List[Data], index: Int): Option[Univ] =
+    def get(ds: Vector[Data], index: Long): Option[Univ] =
       Data.find(ds, index, "#US", "#UM", "#UIM", "#UP", "#UG") flatMap {
-        case Data(_, "#US", List(uid)) => get(ds, uid.toInt) map (Succ(_))
+        case Data(_, "#US", List(uid)) => get(ds, uid.toLong) map (Succ(_))
         case Data(_, "#UM", List(uid1, uid2)) =>
-          for (a <- get(ds, uid1.toInt); b <- get(ds, uid2.toInt)) yield (Max(a, b))
+          for (a <- get(ds, uid1.toLong); b <- get(ds, uid2.toLong)) yield (Max(a, b))
         case Data(_, "#UIM", List(uid1, uid2)) =>
-          for (a <- get(ds, uid1.toInt); b <- get(ds, uid2.toInt)) yield (IMax(a, b))
-        case Data(_, "#UP", List(nid)) => Name.get(ds, nid.toInt) map (Param(_))
-        case Data(_, "#UG", List(nid)) => Name.get(ds, nid.toInt) map (Global(_))
+          for (a <- get(ds, uid1.toLong); b <- get(ds, uid2.toLong)) yield (IMax(a, b))
+        case Data(_, "#UP", List(nid)) => Name.get(ds, nid.toLong) map (Param(_))
+        case Data(_, "#UG", List(nid)) => Name.get(ds, nid.toLong) map (Global(_))
       }
   }
 
@@ -78,34 +80,34 @@ object LeanExportElem {
   sealed trait Expr extends LeanExportElem
 
   object Expr {
-    def get(ds: List[Data], index: Int): Option[Expr] =
+    def get(ds: Vector[Data], index: Long): Option[Expr] =
       Data.find(ds, index, "#EV", "#ES", "#EC", "#EA", "#EL", "#EP") flatMap {
-        case Data(_, "#EV", List(ind)) => Some(Var(ind.toInt))
+        case Data(_, "#EV", List(ind)) => Some(Var(ind.toLong))
         case Data(_, "#ES", List(uid)) =>
-          Univ.get(ds, uid.toInt) map (Sort(_))
+          Univ.get(ds, uid.toLong) map (Sort(_))
         case Data(_, "#EA", List(eid1, eid2)) =>
-          for (a <- get(ds, eid1.toInt); b <- get(ds, eid2.toInt)) yield (Appln(a, b))
+          for (a <- get(ds, eid1.toLong); b <- get(ds, eid2.toLong)) yield (Appln(a, b))
         case Data(_, "#EC", nid :: uids) =>
           {
-            val univs = (uids map ((x) => Univ.get(ds, x.toInt))).flatten
-            Name.get(ds, nid.toInt) map (Const(_, univs))
+            val univs = (uids map ((x) => Univ.get(ds, x.toLong))).flatten
+            Name.get(ds, nid.toLong) map (Const(_, univs))
           }
         case Data(_, "#EL", List(info, nid, eid1, eid2)) =>
           for (
-            a <- Name.get(ds, nid.toInt);
-            b <- get(ds, eid1.toInt);
-            c <- get(ds, eid2.toInt)
+            a <- Name.get(ds, nid.toLong);
+            b <- get(ds, eid1.toLong);
+            c <- get(ds, eid2.toLong)
           ) yield (Lambda(Info.get(info), a, b, c))
 
         case Data(_, "#EP", List(info, nid, eid1, eid2)) =>
           for (
-            a <- Name.get(ds, nid.toInt);
-            b <- get(ds, eid1.toInt);
-            c <- get(ds, eid2.toInt)
+            a <- Name.get(ds, nid.toLong);
+            b <- get(ds, eid1.toLong);
+            c <- get(ds, eid2.toLong)
           ) yield (Pi(Info.get(info), a, b, c))
       }
 
-    case class Var(index: Int) extends Expr
+    case class Var(index: Long) extends Expr
 
     case class Sort(univ: Univ) extends Expr
 
@@ -123,60 +125,79 @@ object LeanExportElem {
   object Import {
     case class Direct(file: Name) extends Import
 
-    case class Relative(backtrack: Int, file: Name) extends Import
+    case class Relative(backtrack: Long, file: Name) extends Import
   }
 
   case class GlobalUniv(name: Name) extends LeanExportElem
 
   object GlobalUniv{
-    def get(ds: List[Data], index: Int) : Option[GlobalUniv] =
+    def get(ds: Vector[Data], index: Long) : Option[GlobalUniv] =
       Data.find(ds, index, "#UNI") flatMap {
         case Data(_, _, List(nid)) =>
-          Name.get(ds, nid.toInt) map (GlobalUniv(_))
+          Name.get(ds, nid.toLong) map (GlobalUniv(_))
       }
   }
 
   case class Definition(name: Name, univParams: List[Name] = List(), tpe: Expr, value: Expr) extends LeanExportElem
 
   object Definition{
-    def get(ds: List[Data], index: Int) : Option[Definition] =
-      Data.find(ds, index, "#DEF") flatMap {
-        case Data(_, _, params) =>
-          val nid = params.head
-          val List(eid1, eid2) = params.takeRight(2)
-          val nids = params.tail.dropRight(2)
-          val univParams = (nids map ((x) => Name.get(ds, x.toInt))).flatten
+    def read(command: String, ds: Vector[Data]) : Option[Definition] = {
+      if (command.startsWith("#DEF"))
+        {
+          val args = command.drop(5)
+          val Array(headArgs, tailArgs) = args split ('|') map ((s) => s.split(' '))
+          val nid  = headArgs.head.toLong
+          val nids = headArgs.tail.map(_.toLong).toList
+          val (eid1, eid2) = (tailArgs(1).toLong, tailArgs(2).toLong)
+          val univParams = (nids map ((x) => Name.get(ds, x.toLong))).flatten
           for (
-            a <- Name.get(ds, nid.toInt);
-            b <- Expr.get(ds, eid1.toInt);
-            c <- Expr.get(ds, eid2.toInt)
+            a <- Name.get(ds, nid.toLong);
+            b <- Expr.get(ds, eid1.toLong);
+            c <- Expr.get(ds, eid2.toLong)
           ) yield (Definition(a, univParams, b, c))
-      }
+        }
+      else None
+    }
+
+    def readAll(lines: Vector[String]) = readDefs(lines, (lines map (Data.read)).flatten)
+
+    def readDefs(lines: Vector[String], ds: Vector[Data]) =
+      (lines map (read(_, ds))).flatten
+
   }
 
   case class Axiom(name: Name, univParams: List[Name] = List(), tpe: Expr) extends LeanExportElem
 
   object Axiom{
-    def get(ds: List[Data], index: Int) : Option[Axiom] =
-      Data.find(ds, index, "#DEF") flatMap {
-        case Data(_, _, params) =>
-          val nid = params.head
-          val eid = params.last
-          val nids = params.tail.init
-          val univParams = (nids map ((x) => Name.get(ds, x.toInt))).flatten
+    def read(command: String, ds: Vector[Data]) : Option[Axiom] = {
+      if (command.startsWith("#DEF"))
+        {
+          val args = command.drop(5)
+          val Array(headArgs, tailArgs) = args split ('|') map ((s) => s.split(' '))
+          val nid  = headArgs.head.toLong
+          val nids = headArgs.tail.map(_.toLong).toList
+          val eid = tailArgs(1).toLong
+          val univParams = (nids map ((x) => Name.get(ds, x.toLong))).flatten
           for (
-            a <- Name.get(ds, nid.toInt);
-            b <- Expr.get(ds, eid.toInt)
+            a <- Name.get(ds, nid.toLong);
+            b <- Expr.get(ds, eid.toLong)
           ) yield (Axiom(a, univParams, b))
-      }
+        }
+      else None
+    }
+
+    def readAll(lines: Vector[String]) = readAxs(lines, (lines map (Data.read)).flatten)
+
+    def readAxs(lines: Vector[String], ds: Vector[Data]) =
+      (lines map (read(_, ds))).flatten
   }
 
-  case class Bind(numParam: Int, numTypes: Int, univParams: List[Int]) extends LeanExportElem
+  case class Bind(numParam: Long, numTypes: Long, univParams: List[Long]) extends LeanExportElem
 
   case object Eind extends LeanExportElem
 
   case class Ind(name: Name, tpe: Expr) extends LeanExportElem
 
-  case class Intro(name: Name, tpe: Expr) extends LeanExportElem
+  case class Longro(name: Name, tpe: Expr) extends LeanExportElem
 
 }
