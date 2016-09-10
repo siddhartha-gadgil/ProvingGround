07ab1c06a1f7b45a67bebdd42add9447a838e6d2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2016-09-10 21:29:17 +0530

    Indexed induction typelevel - hardest case.

---
+++ core/src/main/scala/provingground/IndexedConstructorPatternMap.scala
@@ -0,0 +1,236 @@
+package provingground
+
+import HoTT._
+
+import IterFuncPatternMap.IterFuncPtnMap
+
+import Subst._
+
+abstract class IndexedConstructorPatternMap[
+    S <: Term with Subs[S],
+    Cod <: Term with Subs[Cod],
+    ConstructorType <: Term with Subs[ConstructorType],
+    H <: Term with Subs[H],
+    RecDataType <: Term with Subs[RecDataType],
+    InducDataType <: Term with Subs[InducDataType],
+    F <: Term with Subs[F],
+    Index : Subst,
+    IF <: Term with Subs[IF],
+    IDF <: Term with Subs[IDF],
+    IDFT <: Term with Subs[IDFT]
+] { self =>
+
+  val family : TypFamilyMap[H, F, Cod, Index, IF, IDF, IDFT]
+
+  def subs(x: Term, y: Term): IndexedConstructorPatternMap[
+      S, Cod, ConstructorType, H, RecDataType, InducDataType, F, Index, IF, IDF, IDFT]
+
+
+  /**
+    * returns HoTT type corresponding to the pattern given the (inductive) type W (to be the head).
+    */
+  def apply(tp: F): Typ[ConstructorType]
+
+//  def symbcons(name: AnySym, tp: Typ[H]): ConstructorType =
+//    apply(tp).symbObj(name)
+
+
+  /**
+    * domain containing the recursion data for the constructor, i.e., the HoTT type of recursion data.
+    */
+  def recDataTyp(w: F, x: Typ[Cod]): Typ[RecDataType]
+
+  /**
+    * domain containing the induction data for the constructor, i.e., the HoTT type of the induction data.
+    */
+  def inducDataTyp(w: F, xs: IDFT)(
+      cons: ConstructorType): Typ[InducDataType]
+
+  /**
+    * given a term, matches to see if this is the image of a given (quasi)-constructor, with `this` constructor pattern.
+    * optionally returns simplification (if the term matches), determined by the recursion data.
+    * @param cons constructor, actually quasi-constructor, with which to match.
+    * @param data definition data for the image of the constructor.
+    * @param f the function being defined recursively, to be used recursively in definition.
+    */
+  def recDefCase(cons: ConstructorType,
+                 data: RecDataType,
+                 f: => IF): H => Option[Cod]
+
+  /**
+    * given a term, matches to see if this is the image of a given (quasi)-constructor, with `this` constructor pattern.
+    * optionally returns simplification (if the term matches).
+    * @param cons constructor, actually quasi-constructor, with which to match.
+    * @param data definition data for the image of the constructor.
+    * @param f the function being defined inductively, to be used recursively in definition.
+    */
+  def inducDefCase(cons: ConstructorType,
+                   data: InducDataType,
+                   f: => IDF): H => Option[Cod]
+  val univLevel: Int
+}
+
+object IndexedConstructorPatternMap{
+  case class IndexedIdMap[
+    C <: Term with Subs[C],
+    H <: Term with Subs[H],
+    F <: Term with Subs[F],
+    Index : Subst,
+    IF <: Term with Subs[IF],
+    IDF <: Term with Subs[IDF],
+    IDFT <: Term with Subs[IDFT]](
+        family: TypFamilyMap[H, F, C, Index, IF, IDF, IDFT],
+        index: Index)
+    extends IndexedConstructorPatternMap[
+      HeadTerm, C, H, H, C, C, F, Index, IF, IDF, IDFT] {
+
+  def apply(W: F) = family.pattern.typ(W, index)
+
+  val univLevel = 0
+
+  def recDataTyp(w: F, x: Typ[C]) = x
+
+  def inducDataTyp(w: F, xs: IDFT)(
+      cons: H) = family.typRestrict(xs, index)(cons)
+
+  def subs(x: Term, y: Term) = this
+
+  def recDefCase(cons: H, data: C, f: => IF): Term => Option[C] = {
+    case (t: Term) if t == cons => Some(data)
+    case _ => None
+  }
+
+  def inducDefCase(cons: H, data: C, f: => IDF): Term => Option[C] = {
+    case (t: Term) if t == cons => Some(data)
+    case _ => None
+  }
+}
+
+  abstract class IndexedRecursiveConstructorPatternMap[
+    HS <: Term with Subs[HS],
+    S <: Term with Subs[S],
+    Cod <: Term with Subs[Cod],
+    ArgType <: Term with Subs[ArgType],
+    HeadConstructorType <: Term with Subs[HeadConstructorType],
+    CT <: FuncLike[ArgType, HeadConstructorType] with Subs[CT],
+    H <: Term with Subs[H],
+    RecDataType <: Term with Subs[RecDataType],
+    InducDataType <: Term with Subs[InducDataType],
+    HeadRecDataType <: Term with Subs[HeadRecDataType],
+    HeadInducDataType <: Term with Subs[HeadInducDataType],
+    F <: Term with Subs[F],
+    Index : Subst,
+    IF <: Term with Subs[IF],
+    IDF <: Term with Subs[IDF],
+    IDFT <: Term with Subs[IDFT]]
+    extends IndexedConstructorPatternMap[
+      S, Cod, CT, H, RecDataType, InducDataType, F, Index, IF, IDF, IDFT
+      ] {
+  self =>
+
+  /**
+    * The head pattern, constant T for A -> T and T(a) for A ~> T(a)
+    */
+  val headfibre: ArgType => IndexedConstructorPatternMap[
+      HS, Cod, HeadConstructorType, H, HeadRecDataType, HeadInducDataType, F, Index, IF, IDF, IDFT]
+
+  /**
+    * returns data for recursion to be passed on to the head given an argument (when matching with the constructor).
+    */
+  def headData(
+      data: RecDataType, arg: ArgType, f: => IF): HeadRecDataType
+
+  def recDefCase(
+      cons: CT, data: RecDataType, f: => IF): H => Option[Cod] = {
+    t =>
+      for (arg <- getArg(cons)(t);
+           term <- headfibre(arg).recDefCase(
+                      cons(arg), headData(data, arg, f), f)(t)) yield term
+  }
+
+  def headInducData(data: InducDataType,
+                    arg: ArgType,
+                    f: => IDF): HeadInducDataType
+
+  def inducDefCase(cons: CT,
+                   data: InducDataType,
+                   f: => IDF): H => Option[Cod] = { t =>
+    for (arg <- getArg(cons)(t);
+         term <- headfibre(arg).inducDefCase(
+                    cons(arg), headInducData(data, arg, f), f)(t)) yield term
+  }
+}
+
+  case class IndexedFuncPtnMap[HS <: Term with Subs[HS],
+                      TS <: Term with Subs[TS],
+                      C <: Term with Subs[C],
+                      F <: Term with Subs[F],
+                      HC <: Term with Subs[HC],
+                      H <: Term with Subs[H],
+                      HR <: Term with Subs[HR],
+                      HI <: Term with Subs[HI],
+                      TT <: Term with Subs[TT],
+                      DT <: Term with Subs[DT],
+                      Fb <: Term with Subs[Fb],
+    Index : Subst,
+    IF <: Term with Subs[IF],
+    IDF <: Term with Subs[IDF],
+    IDFT <: Term with Subs[IDFT]](
+    tail: IterFuncPtnMap[TS, H, C, F, TT, DT],
+    head: IndexedConstructorPatternMap[HS, C, HC, H, HR, HI, Fb, Index, IF, IDF, IDFT],
+    ind: Index
+)
+    extends IndexedRecursiveConstructorPatternMap[HS,
+                                           Func[TS, HS],
+                                           C,
+                                           F,
+                                           HC,
+                                           Func[F, HC],
+                                           H,
+                                           Func[F, Func[TT, HR]],
+                                           FuncLike[F, Func[DT, HI]],
+                                           HR,
+                                           HI,
+                                           Fb, Index, IF, IDF, IDFT] { self =>
+
+
+
+
+ def subs(x: Term, y: Term) =
+   IndexedFuncPtnMap(tail.subs(x, y), head.subs(x, y), ind.subst(x, y))
+
+  val family = head.family
+
+  val headfibre = (t: F) => head
+
+  //    type ConstructorType = Func[ArgType, head.ConstructorType]
+
+  def recDataTyp(w: Fb, x: Typ[C]) =
+    tail(family.pattern.typ(w,ind)) ->: tail.target(x) ->: head.recDataTyp(w, x)
+
+  def inducDataTyp(w: Fb, xs: IDFT)(
+      cons: Func[F, HC]): Typ[FuncLike[F, Func[DT, HI]]] = {
+    val a = tail(family.pattern.typ(w,ind)).Var
+    val headcons = cons(a)
+    val xss = family.typRestrict(xs, ind)
+    val fibre =
+      lmbda(a)(tail.depTarget(xss)(a) ->: head.inducDataTyp(w, xs)(headcons))
+    PiTyp(fibre)
+  }
+
+  def headData(data: Func[F, Func[TT, HR]], arg: F, f: => IF): HR = {
+    data(arg)(tail.induced(family.restrict(f, ind))(arg))
+  }
+
+  def headInducData(data: FuncLike[F, Func[DT, HI]],
+                    arg: F,
+                    f: => IDF): HI = {
+    data(arg)(tail.inducedDep(family.depRestrict(f, ind))(arg))
+  }
+
+  def apply(W: Fb) =
+    tail(family.pattern.typ(W, ind)) ->:  head(W)
+
+  val univLevel = math.max(head.univLevel, tail.univLevel)
+}
+}
--- core/src/main/scala/provingground/TypFamily.scala
+++ core/src/main/scala/provingground/TypFamily.scala
@@ -2,76 +2,106 @@ package provingground
 
 import HoTT._
 
-sealed trait TypFamilyPtn[H <: Term with Subs[H], F <: Term with Subs[F], Index] {
+trait Subst[A]{
+  def subst(a: A)(x: Term, y: Term): A
+}
+
+object Subst{
+  implicit def  termSubst[U <: Term with Subs[U]] : Subst[U] =
+    new Subst[U]{
+    def subst(a: U)(x: Term, y: Term) = a.replace(x, y)
+  }
+
+  implicit object UnitSubst extends Subst[Unit]{
+    def subst(a: Unit)(x: Term, y: Term) = a
+  }
+
+  implicit def pairSubst[U : Subst, V: Subst] : Subst[(U, V)]  =
+    new Subst[(U, V)]{
+      def subst(a: (U, V))(x: Term, y: Term) =
+        ( implicitly[Subst[U]].subst(a._1)(x, y),
+          implicitly[Subst[V]].subst(a._2)(x, y))
+  }
+
+  implicit class SubstOp[A: Subst](a: A){
+    def subst(x: Term, y: Term) = implicitly[Subst[A]].subst(a)(x, y) 
+}
+}
+
+object TestS{ implicitly[Subst[(Unit, Term)]]}
+
+
+sealed abstract class TypFamilyPtn[H <: Term with Subs[H], F <: Term with Subs[F], Index: Subst] {
   def getIndex(w: F, typ: Typ[H]) : Option[Index]
-  
+
   def typ(w: F, index: Index): Typ[H]
-  
-  def subs(ind: Index)(x: Term, y: Term) : Index
-  
-  def mapper[C <: Term with Subs[C]] : 
+
+  def subs(ind: Index)(x: Term, y: Term) : Index // not needed, use `Subst`
+
+  def mapper[C <: Term with Subs[C]] :
     TypFamilyMapper[H, F, C, Index, IF, IDF, IDFT] forSome{
       type IF <: Term with Subs[IF];
       type IDF <: Term with Subs[IDF];
       type IDFT <: Term with Subs[IDFT]
   }
-  
+
   def mapped[C <: Term with Subs[C]] = mapper[C].mapper(this)
 }
 
 object TypFamilyPtn{
   import TypFamilyMapper._
-  
+
   case class IdTypFamily[H <: Term with Subs[H]]() extends TypFamilyPtn[H, Typ[H], Unit]{
     def getIndex(w: Typ[H], typ: Typ[H]) = Some(())
-  
+
     def typ(w: Typ[H], index: Unit): Typ[H] = w
-  
+
     def subs(ind: Unit)(x: Term, y: Term)  = ind
-    
+
     def mapper[C <: Term with Subs[C]] = idTypFamilyMapper[H, C]
-    
-    
+
+
   }
-  
+
   case class FuncTypFamily[
     U<: Term with Subs[U],
-    H <: Term with Subs[H], TF <: Term with Subs[TF], TI](
+    H <: Term with Subs[H], TF <: Term with Subs[TF], TI : Subst](
     head: Typ[U], tail: TypFamilyPtn[H, TF, TI]) extends TypFamilyPtn[H, Func[U, TF], (U, TI)]{
-    
+
     def getIndex(w: Func[U, TF], typ: Typ[H]) = {
       val argOpt = getArg(w)(typ)
       argOpt flatMap {(arg) => tail.getIndex(w(arg), typ).map((arg, _))}
     }
-    
+
     def typ(w: Func[U, TF], index: (U, TI)): Typ[H] = tail.typ(w(index._1), index._2)
-  
+
     def subs(ind: (U, TI))(x: Term, y: Term) = (ind._1.replace(x, y), tail.subs(ind._2)(x, y))
 
-    def mapper[C <: Term with Subs[C]] = funcTypFamilyMapper(tail.mapper[C])
+    def mapper[C <: Term with Subs[C]] =
+      funcTypFamilyMapper(tail.mapper[C], implicitly[Subst[TI]])
   }
-  
+
   case class DepFuncTypFamily[
     U<: Term with Subs[U],
-    H <: Term with Subs[H], TF <: Term with Subs[TF], TI](
+    H <: Term with Subs[H], TF <: Term with Subs[TF], TI : Subst](
     head: Typ[U], tailfibre: U =>TypFamilyPtn[H, TF, TI]) extends TypFamilyPtn[H, Func[U, TF], (U, TI)]{
-    
+
     def getIndex(w: Func[U, TF], typ: Typ[H]) = {
       val argOpt = getArg(w)(typ)
       argOpt flatMap {(arg) => tailfibre(arg).getIndex(w(arg), typ).map((arg, _))}
     }
-    
-    def typ(w: Func[U, TF], index: (U, TI)): Typ[H] = 
+
+    def typ(w: Func[U, TF], index: (U, TI)): Typ[H] =
       tailfibre(index._1).typ(w(index._1), index._2)
-  
-    def subs(ind: (U, TI))(x: Term, y: Term) = 
+
+    def subs(ind: (U, TI))(x: Term, y: Term) =
       (ind._1.replace(x, y), tailfibre(ind._1).subs(ind._2)(x, y))
-  
-    def mapper[C <: Term with Subs[C]] = 
-      depFuncTypFamilyMapper(tailfibre(head.Var).mapper[C])
+
+    def mapper[C <: Term with Subs[C]] =
+      depFuncTypFamilyMapper(tailfibre(head.Var).mapper[C], implicitly[Subst[TI]])
   }
-    
-  
+
+
 }
 
 sealed trait TypFamilyMap[
@@ -82,39 +112,53 @@ sealed trait TypFamilyMap[
     IF <: Term with Subs[IF],
     IDF <: Term with Subs[IDF],
     IDFT <: Term with Subs[IDFT]]{
-    
+
+    val pattern: TypFamilyPtn[H, F, Index]
+
     def iterFuncTyp(w: Typ[H], x : Typ[C]) : Typ[IF]
-    
+
     def iterDepFuncTyp(w: Typ[H], xs: IDFT) : Typ[IDF]
-    
+
     def restrict(f: IF, ind: Index) : Func[H, C]
-    
+
     def depRestrict(f: IDF, ind: Index): FuncLike[H, C]
+
+    def typRestrict(xs: IDFT, ind: Index) : Func[H, Typ[C]]
+
+    def subs(x: Term, y: Term) : TypFamilyMap[H, F, C, Index, IF, IDF, IDFT]
   }
-  
+
 object TypFamilyMap{
-  
+
+  import TypFamilyPtn._
+
   case class IdTypFamilyMap[
     H<: Term with Subs[H],
-    C<: Term with Subs[C]]() extends 
+    C<: Term with Subs[C]]() extends
     TypFamilyMap[
       H, Typ[H], C, Unit, Func[H, C], FuncLike[H, C], Func[H, Typ[C]]]{
-    
+
+    val pattern = IdTypFamily[H]
+
     def iterFuncTyp(w: Typ[H], x: Typ[C]) = w ->: x
-    
+
     def iterDepFuncTyp(w: Typ[H], xs: Func[H, Typ[C]]) = PiTyp(xs)
-    
+
     def restrict(f: Func[H, C], ind: Unit) = f
-    
+
     def depRestrict(f: FuncLike[H, C], ind: Unit) = f
+
+    def typRestrict(xs: Func[H, Typ[C]], ind: Unit) = xs
+
+    def subs(x: Term, y: Term) = this
   }
-  
+
   case class FuncTypFamilyMap[
     U <: Term with Subs[U],
     H <: Term with Subs[H],
     TF <: Term with Subs[TF],
     C <: Term with Subs[C],
-    TIndex,
+    TIndex : Subst,
     TIF <: Term with Subs[TIF],
     TIDF <: Term with Subs[TIDF],
     TIDFT <: Term with Subs[TIDFT]](
@@ -122,26 +166,33 @@ object TypFamilyMap{
         tail : TypFamilyMap[H, TF, C, TIndex, TIF, TIDF, TIDFT]) extends
         TypFamilyMap[
           H, Func[U, TF], C, (U, TIndex), Func[U, TIF], FuncLike[U, TIDF], FuncLike[U, TIDFT]]{
- 
+
+    val pattern = FuncTypFamily(head, tail.pattern)
+
     def iterFuncTyp(w: Typ[H], x : Typ[C]) = head ->: tail.iterFuncTyp(w, x)
-    
+
     def iterDepFuncTyp(w: Typ[H], xs: FuncLike[U, TIDFT]) = {
       val x = head.Var
       x ~>: (tail.iterDepFuncTyp(w, xs(x)))
     }
-    
+
     def restrict(f: Func[U, TIF], ind: (U, TIndex)) = tail.restrict(f(ind._1), ind._2)
-    
-    def depRestrict(f: FuncLike[U, TIDF], ind: (U, TIndex)) = 
+
+    def depRestrict(f: FuncLike[U, TIDF], ind: (U, TIndex)) =
       tail.depRestrict(f(ind._1), ind._2)
+
+    def typRestrict(xs: FuncLike[U, TIDFT], ind: (U, TIndex)) =
+      tail.typRestrict(xs(ind._1), ind._2)
+
+    def subs(x: Term, y: Term) = FuncTypFamilyMap(head.replace(x, y), tail.subs(x,y))
   }
-  
+
   case class DepFuncTypFamilyMap[
     U <: Term with Subs[U],
     H <: Term with Subs[H],
     TF <: Term with Subs[TF],
     C <: Term with Subs[C],
-    TIndex,
+    TIndex : Subst,
     TIF <: Term with Subs[TIF],
     TIDF <: Term with Subs[TIDF],
     TIDFT <: Term with Subs[TIDFT]](
@@ -149,25 +200,33 @@ object TypFamilyMap{
         tailfibre : U => TypFamilyMap[H, TF, C, TIndex, TIF, TIDF, TIDFT]) extends
         TypFamilyMap[
           H, Func[U, TF], C, (U, TIndex), FuncLike[U, TIF], FuncLike[U, TIDF], FuncLike[U, TIDFT]]{
- 
-    def iterFuncTyp(w: Typ[H], x : Typ[C]) = 
+
+    val pattern = DepFuncTypFamily(head, (u: U) => tailfibre(u).pattern)
+
+    def iterFuncTyp(w: Typ[H], x : Typ[C]) =
       {
       val y = head.Var
       y ~>: tailfibre(y).iterFuncTyp(w, x)
       }
-    
+
     def iterDepFuncTyp(w: Typ[H], xs: FuncLike[U, TIDFT]) = {
       val x = head.Var
       x ~>: (tailfibre(x).iterDepFuncTyp(w, xs(x)))
     }
-    
-    def restrict(f: FuncLike[U, TIF], ind: (U, TIndex)) = 
+
+    def restrict(f: FuncLike[U, TIF], ind: (U, TIndex)) =
       tailfibre(ind._1).restrict(f(ind._1), ind._2)
-    
-    def depRestrict(f: FuncLike[U, TIDF], ind: (U, TIndex)) = 
+
+    def depRestrict(f: FuncLike[U, TIDF], ind: (U, TIndex)) =
       tailfibre(ind._1).depRestrict(f(ind._1), ind._2)
+
+     def typRestrict(xs: FuncLike[U, TIDFT], ind: (U, TIndex)) =
+      tailfibre(ind._1).typRestrict(xs(ind._1), ind._2)
+
+     def subs(x: Term, y: Term) =
+       DepFuncTypFamilyMap(head.replace(x, y), (u: U) => tailfibre(u).subs(x, y))
   }
-  
+
 }
 
 sealed trait TypFamilyMapper[
@@ -178,39 +237,40 @@ sealed trait TypFamilyMapper[
     IF <: Term with Subs[IF],
     IDF <: Term with Subs[IDF],
     IDFT <: Term with Subs[IDFT]]{
-  
-  val mapper : TypFamilyPtn[H, F, Index] => TypFamilyMap[H, F, C, Index, IF, IDF, IDFT]  
+
+  val mapper : TypFamilyPtn[H, F, Index] => TypFamilyMap[H, F, C, Index, IF, IDF, IDFT]
 }
 
 object TypFamilyMapper{
   import TypFamilyMap._
-  
+
   import TypFamilyPtn._
-  
-  implicit def idTypFamilyMapper[H <: Term with Subs[H], C <: Term with Subs[C]] = 
+
+  implicit def idTypFamilyMapper[H <: Term with Subs[H], C <: Term with Subs[C]] =
     new TypFamilyMapper[H, Typ[H], C, Unit, Func[H, C], FuncLike[H, C], Func[H, Typ[C]]]{
-      val  mapper = (x: TypFamilyPtn[H, Typ[H], Unit]) => IdTypFamilyMap[H, C] 
+      val  mapper = (x: TypFamilyPtn[H, Typ[H], Unit]) => IdTypFamilyMap[H, C]
   }
-  
+
   implicit def funcTypFamilyMapper[
     U <: Term with Subs[U],
     H <: Term with Subs[H],
     TF <: Term with Subs[TF],
     C <: Term with Subs[C],
-    TIndex,
+    TIndex ,
     TIF <: Term with Subs[TIF],
     TIDF <: Term with Subs[TIDF],
     TIDFT <: Term with Subs[TIDFT]](
-        implicit tail : TypFamilyMapper[H, TF, C, TIndex, TIF, TIDF, TIDFT]) = 
+        implicit tail : TypFamilyMapper[H, TF, C, TIndex, TIF, TIDF, TIDFT],
+        subst: Subst[TIndex]) =
       new TypFamilyMapper[
           H, Func[U, TF], C, (U, TIndex), Func[U, TIF], FuncLike[U, TIDF], FuncLike[U, TIDFT]]{
-    
+
     val mapper : TypFamilyPtn[H, Func[U, TF], (U, TIndex)] => TypFamilyMap[
           H, Func[U, TF], C, (U, TIndex), Func[U, TIF], FuncLike[U, TIDF], FuncLike[U, TIDFT]] =
-      {case FuncTypFamily(h , t) => 
+      {case FuncTypFamily(h , t) =>
         FuncTypFamilyMap(h, tail.mapper(t))}
   }
-  
+
   implicit def depFuncTypFamilyMapper[
     U <: Term with Subs[U],
     H <: Term with Subs[H],
@@ -220,13 +280,13 @@ object TypFamilyMapper{
     TIF <: Term with Subs[TIF],
     TIDF <: Term with Subs[TIDF],
     TIDFT <: Term with Subs[TIDFT]](
-        implicit tail : TypFamilyMapper[H, TF, C, TIndex, TIF, TIDF, TIDFT]) = 
+        implicit tail : TypFamilyMapper[H, TF, C, TIndex, TIF, TIDF, TIDFT], subst: Subst[TIndex]) =
       new TypFamilyMapper[
           H, Func[U, TF], C, (U, TIndex), FuncLike[U, TIF], FuncLike[U, TIDF], FuncLike[U, TIDFT]]{
-    
+
     val mapper : TypFamilyPtn[H, Func[U, TF], (U, TIndex)] => TypFamilyMap[
           H, Func[U, TF], C, (U, TIndex), FuncLike[U, TIF], FuncLike[U, TIDF], FuncLike[U, TIDFT]] =
-      {case DepFuncTypFamily(h , tf) => 
+      {case DepFuncTypFamily(h , tf) =>
         DepFuncTypFamilyMap(h, (u: U) => tail.mapper(tf(u)))}
   }
 }
