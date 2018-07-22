60a4f250584f4c2b76e79352645c7b6ece769799
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-07-22 17:44:57 +0530

    Random Variable, not Sort

--- core/src/main/scala/provingground/learning/TermGenerator.scala
+++ core/src/main/scala/provingground/learning/TermGenerator.scala
@@ -14,31 +14,31 @@ import shapeless._, HList._
 import scala.language.higherKinds
 
 /**
-  * A sort, corresponding to which we get a distribution from an existing one.
+  * A random variable, corresponding to which we get a distribution from an existing one.
   * Can also be used for conditioning, giving one distribution from another.
   */
-sealed trait Sort[S, T]
+sealed trait RandomVar[S, T]
 
-object Sort {
-  case class True[S]() extends Sort[S, S]
+object RandomVar {
+  case class True[S]() extends RandomVar[S, S]
 
-  case class Filter[S](pred: S => Boolean) extends Sort[S, S]
+  case class Filter[S](pred: S => Boolean) extends RandomVar[S, S]
 
-  case class Restrict[S, T](optMap: S => Option[T]) extends Sort[S, T]
+  case class Restrict[S, T](optMap: S => Option[T]) extends RandomVar[S, T]
 
   val AllTerms = True[Term]
 }
 
-sealed trait SortList[S, U <: HList] {
-  def ::[X](that: Sort[S, X]) = SortList.Cons(that, this)
+sealed trait RandomVarList[S, U <: HList] {
+  def ::[X](that: RandomVar[S, X]) = RandomVarList.Cons(that, this)
 }
 
-object SortList {
-  case class Nil[S]() extends SortList[S, HNil]
+object RandomVarList {
+  case class Nil[S]() extends RandomVarList[S, HNil]
 
-  case class Cons[S, X, U <: HList](head: Sort[S, X],
-                                    tail: SortList[S, U])
-      extends SortList[S, X :: U]
+  case class Cons[S, X, U <: HList](head: RandomVar[S, X],
+                                    tail: RandomVarList[S, U])
+      extends RandomVarList[S, X :: U]
 }
 
 /**
@@ -46,72 +46,72 @@ object SortList {
   * encoded as an HList, but has just one output.
   */
 sealed trait GeneratorNode[S,  O] {
-  val codomain: Sort[S, O]
+  val codomain: RandomVar[S, O]
 }
 
 sealed trait BaseGeneratorNode[S, I<: HList, O] extends GeneratorNode[S, O]{
-  val polyDomain: SortList[S, I]
+  val polyDomain: RandomVarList[S, I]
 }
 
 object GeneratorNode {
-  case class Init[S, X](domain: Sort[S, X]) extends BaseGeneratorNode[S, X :: HNil, X]{
+  case class Init[S, X](domain: RandomVar[S, X]) extends BaseGeneratorNode[S, X :: HNil, X]{
     val codomain = domain
-    val polyDomain = domain :: SortList.Nil[S]
+    val polyDomain = domain :: RandomVarList.Nil[S]
   }
 
-  case class ConditionedInit[S, X, Y](domain: Sort[S, X], codomain: Sort[S, Y]
+  case class ConditionedInit[S, X, Y](domain: RandomVar[S, X], codomain: RandomVar[S, Y]
   ) extends BaseGeneratorNode[S, X :: HNil, Y]{
-    val polyDomain = domain :: SortList.Nil[S]
+    val polyDomain = domain :: RandomVarList.Nil[S]
   }
 
   case class Map[S, X, Y](f: X => Y,
-                          domain: Sort[S, X],
-                          codomain: Sort[S, Y])
+                          domain: RandomVar[S, X],
+                          codomain: RandomVar[S, Y])
       extends BaseGeneratorNode[S, X :: HNil, Y] {
-    val polyDomain = domain :: SortList.Nil[S]
+    val polyDomain = domain :: RandomVarList.Nil[S]
   }
 
   case class MapOpt[S, X, Y](f: X => Option[Y],
-                             domain: Sort[S, X],
-                             codomain: Sort[S, Y])
+                             domain: RandomVar[S, X],
+                             codomain: RandomVar[S, Y])
       extends BaseGeneratorNode[S, X :: HNil, Y] {
-    val polyDomain = domain :: SortList.Nil[S]
+    val polyDomain = domain :: RandomVarList.Nil[S]
   }
 
   case class ZipMap[S, X1, X2, Y](f: (X1, X2) => Y,
-                                  domain1: Sort[S, X1],
-                                  domain2: Sort[S, X2],
-                                  codomain: Sort[S, Y])
+                                  domain1: RandomVar[S, X1],
+                                  domain2: RandomVar[S, X2],
+                                  codomain: RandomVar[S, Y])
       extends BaseGeneratorNode[S, X1 :: X2 :: HNil, Y] {
-    val polyDomain = domain1 :: domain2 :: SortList.Nil[S]
+    val polyDomain = domain1 :: domain2 :: RandomVarList.Nil[S]
   }
 
   case class ZipMapOpt[S, X1, X2, Y](f: (X1, X2) => Option[Y],
-                                     domain1: Sort[S, X1],
-                                     domain2: Sort[S, X2],
-                                     codomain: Sort[S, Y])
+                                     domain1: RandomVar[S, X1],
+                                     domain2: RandomVar[S, X2],
+                                     codomain: RandomVar[S, Y])
       extends BaseGeneratorNode[S, X1 :: X2 :: HNil, Y] {
-    val polyDomain = domain1 :: domain2 :: SortList.Nil[S]
+    val polyDomain = domain1 :: domain2 :: RandomVarList.Nil[S]
   }
 
   case class FiberProduct[S, X1, X2, Z, Y](
                                   quot: X1 => Z,
-                                  fiberDomain: Y => Sort[S, X2],
+                                  fiberDomain: Y => RandomVar[S, X2],
                                   f: (X1, X2) => Y,
-                                  baseDomain: Sort[S, X1],
-                                  codomain: Sort[S, Y])
+                                  baseDomain: RandomVar[S, X1],
+                                  codomain: RandomVar[S, Y])
       extends GeneratorNode[S, Y]
 
   case class ThenCondition[S, O, Y](
       gen: GeneratorNode[S,  O],
-      condition: Sort[S, Y]
+      condition: RandomVar[S, Y]
   ) extends GeneratorNode[S, Y] {
     val codomain   = condition
   }
 
   case class Island[S, O, Y, State, Boat, D[_]](
-      islandCodomain: Sort[S, O],
-      codomain: Sort[S, Y],
+      islandCodomain: RandomVar[S, O],
+      codomain: RandomVar[S, Y],
       initMap: State => (State, Boat),
       export: (Boat, O) => Y
   )(implicit dists: DistributionState[S, State, D]) extends GeneratorNode[S, Y]
@@ -123,29 +123,29 @@ object GeneratorNode {
  * modifying a state from distributions
  */
 trait DistributionState[S, State, D[_]]{
-  def distributions[T](sort: Sort[S, T])(state : State) : D[T]
+  def distributions[T](randomVar: RandomVar[S, T])(state : State) : D[T]
 
   def update(dists: Distributions[S, D])(init: State): State
 }
 
 trait Distributions[S, D[_]]{
-  def dist[T](sort: Sort[S, T]) : D[S]
+  def dist[T](randomVar: RandomVar[S, T]) : D[S]
 }
 
 /**
  * typeclass for being able to condition
  */
 trait Conditioning[D[_]]{
-  def condition[S, T](c: Sort[S, T]) : D[S] => D[T]
+  def condition[S, T](c: RandomVar[S, T]) : D[S] => D[T]
 }
 
 case object Conditioning{
   def flatten[S, D[_]](implicit cd: Conditioning[D]) : D[Option[S]] => D[S] =
-    cd.condition(Sort.Restrict[Option[S], S](identity))
+    cd.condition(RandomVar.Restrict[Option[S], S](identity))
 }
 
-object TermSorts{
-  import Sort._
+object TermRandomVars{
+  import RandomVar._
 
   val terms = True[Term]
 
