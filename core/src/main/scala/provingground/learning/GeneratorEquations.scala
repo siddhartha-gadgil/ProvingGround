afa166ffca159fdcf1c78075afd9a77020b8d554
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-08-11 15:11:18 +0530

    more equation gen

--- core/src/main/scala/provingground/learning/GeneratorEquations.scala
+++ core/src/main/scala/provingground/learning/GeneratorEquations.scala
@@ -51,10 +51,34 @@ case class GeneratorEquations[State, Boat](
         val eqTerms =
           finalProbs(input).map {
             case (x, p) =>
-              EquationTerm(FinalVal(Elem(x, output)), p)
+              EquationTerm(FinalVal(Elem(f(x), output)), p)
           }
         (eqTerms, Set())
-
+      case MapOpt(f, input, output) =>
+        val eqTerms =
+          finalProbs(input).flatMap {
+            case (x, p) =>
+              f(x).map { y =>
+                val condition = FinalVal(Event(input, Sort.Restrict(f)))
+                EquationTerm(FinalVal(Elem(y, output)), p / condition)
+              } }
+        (eqTerms, Set())
+      case ZipMap(f, input1, input2, output) =>
+        val eqTerms =
+          for {
+            (x1, p1) <- finalProbs(input1)
+            (x2, p2) <- finalProbs(input2)
+          } yield EquationTerm(FinalVal(Elem(f(x1, x2), output)), p1 * p2)
+        (eqTerms, Set())
+      case zm: ZipMapOpt[o1, o2, Y] =>
+        val eqTerms =
+          for {
+            (x1, p1) <- finalProbs(zm.input1)
+            (x2, p2) <- finalProbs(zm.input2)
+            condition = FinalVal(PairEvent(zm.input1, zm.input2, Sort.Restrict[(o1, o2), Y]{case (x1, x2) => zm.f(x1, x2)}))
+            y <- zm.f(x1, x2)
+          } yield EquationTerm(FinalVal(Elem(y, zm.output)), p1 * p2 /condition)
+        (eqTerms, Set())
       case _ => ???
     }
 }
