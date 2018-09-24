cd2f6910007159bc633053e38bd9224c830f8547
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-09-24 10:34:54 +0530

    helpers for pure websocket task interactions

---
+++ core/src/main/scala/provingground/interface/MultiTask.scala
@@ -0,0 +1,30 @@
+package provingground.interface
+
+import upickle.Js
+import upickle.default._
+import monix.eval._
+
+import scala.util.{Failure, Success}
+
+case class MultiTask(jobs: Map[String, String => Task[String]]) extends (String => Task[String]){
+  def apply(inp: String) : Task[String] = {
+    val obj: Js.Obj = read[Js.Obj](inp)
+    val jobName =  obj("job").str
+    val job: String => Task[String] = jobs.getOrElse(jobName, (_) => Task.raiseError(new IllegalArgumentException(s"Cannot find job with name $jobName")))
+    val data = obj("data").str
+    job(data).materialize.map{
+      case Success(result: String) =>
+        write[Js.Obj](
+          Js.Obj("job" -> Js.Str(jobName), "data" -> Js.Str(data), "result" -> Js.Str(result), "success" -> Js.Bool(true))
+        )
+      case Failure(err: Throwable) =>
+        write[Js.Obj](
+          Js.Obj("job" -> Js.Str(jobName), "data" -> Js.Str(data), "error" -> Js.Str(err.toString), "success" -> Js.Bool(false))
+        )
+    }
+  }
+
+  def +(kv: (String, String => Task[String])): MultiTask = MultiTask(jobs + kv)
+
+  def ++(that: MultiTask): MultiTask = MultiTask(jobs ++ that.jobs)
+}
--- mantle/src/main/scala/provingground/MantleCask.scala
+++ mantle/src/main/scala/provingground/MantleCask.scala
@@ -2,13 +2,14 @@ package provingground.interface
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import MantleService._
-
 import io.undertow.websockets.WebSocketConnectionCallback
 import io.undertow.websockets.core.{AbstractReceiveListener, BufferedTextMessage, WebSocketChannel, WebSockets}
 import io.undertow.websockets.spi.WebSocketHttpExchange
+import monix.execution.CancelableFuture
+
 import scala.util.Try
 
-object MantleRoutes extends cask.Routes{
+object MantleRoutes extends cask.Routes {
   @cask.staticFiles("docs")
   def docsRoute() = "docs"
 
@@ -16,28 +17,20 @@ object MantleRoutes extends cask.Routes{
   def public() = "."
 
   @cask.get("/")
-  def root() = Site.page(indexHTML,
-            "resources/",
-            "ProvingGround HoTT Server",
-            false)
+  def root() =
+    Site.page(indexHTML, "resources/", "ProvingGround HoTT Server", false)
 
   @cask.get("/index.html")
-  def index() = Site.page(indexHTML,
-            "resources/",
-            "ProvingGround HoTT Server",
-            false)
+  def index() =
+    Site.page(indexHTML, "resources/", "ProvingGround HoTT Server", false)
 
   @cask.get("/prover.html")
-  def prover() = Site.page(proverHTML,
-            "resources/",
-            "ProvingGround HoTT Server",
-            false)
+  def prover() =
+    Site.page(proverHTML, "resources/", "ProvingGround HoTT Server", false)
 
   @cask.get("/leanlib.html")
-  def leanlib() = Site.page(leanlibHTML,
-            "resources/",
-            "ProvingGround Lean Export",
-            false)
+  def leanlib() =
+    Site.page(leanlibHTML, "resources/", "ProvingGround Lean Export", false)
 
   @cask.post("/monoid-proof")
   def seek() = {
@@ -55,53 +48,82 @@ object MantleRoutes extends cask.Routes{
   def fiddle() = AmmService.indexHTML
 
   @cask.post("/scripts/kernel")
-  def repl(request: cask.Request) =
-    {
-      val d = new String(request.readAllBytes())
-      println(s"post received:\n$d")
+  def repl(request: cask.Request) = {
+    val d = new String(request.readAllBytes())
+    println(s"post received:\n$d")
 
-      val result =
-        AmmService.replResult(d) match {
-          case Right(z) => "--RESULT--\n" + z
-          case Left(z)  => "--ERROR--\n" + z
-        }
+    val result =
+      AmmService.replResult(d) match {
+        case Right(z) => "--RESULT--\n" + z
+        case Left(z)  => "--ERROR--\n" + z
+      }
 
-        result
-    }
+    result
+  }
 
   def sendProof(channel: WebSocketChannel): Unit = {
     val pfFut = MonoidServer.seekResultFut
     pfFut.foreach((pf) => WebSockets.sendTextBlocking(pf.toString, channel))
   }
-
-
   @cask.websocket("/monoid-websock")
-    def showUserProfile(): cask.WebsocketResult = {
-      new WebSocketConnectionCallback() {
-        override def onConnect(exchange: WebSocketHttpExchange, channel: WebSocketChannel): Unit = {
-          channel.getReceiveSetter.set(
-            new AbstractReceiveListener() {
-              override def onFullTextMessage(channel: WebSocketChannel, message: BufferedTextMessage): Unit = {
-                message.getData match{
-                  case "" => channel.close()
-                  case data =>
-                    pprint.log(s"received $data over websocket")
-                    sendProof(channel)
-                    WebSockets.sendTextBlocking(data, channel)
-                }
+  def showUserProfile(): cask.WebsocketResult = {
+    new WebSocketConnectionCallback() {
+      override def onConnect(exchange: WebSocketHttpExchange,
+                             channel: WebSocketChannel): Unit = {
+        channel.getReceiveSetter.set(
+          new AbstractReceiveListener() {
+            override def onFullTextMessage(
+                channel: WebSocketChannel,
+                message: BufferedTextMessage): Unit = {
+              message.getData match {
+                case "" => channel.close()
+                case data =>
+                  pprint.log(s"received $data over websocket")
+                  sendProof(channel)
+                  WebSockets.sendTextBlocking(data, channel)
               }
             }
-          )
-          channel.resumeReceives()
-        }
+          }
+        )
+        channel.resumeReceives()
       }
     }
+  }
 
   initialize()
 
 }
 
-object MantleCask extends cask.Main(MantleRoutes, LeanRoutes){
+object MantleCask extends cask.Main(MantleRoutes, LeanRoutes) {
   override def port = Try(sys.env("PROVINGGROUND_PORT").toInt).getOrElse(8080)
   override def host = Try(sys.env("PROVINGGROUND_HOST")).getOrElse("localhost")
+}
+
+import monix.eval._
+
+
+class TaskSocket(task: String => Task[String])
+    extends WebSocketConnectionCallback() {
+
+  import monix.execution.Scheduler.Implicits.global
+
+  def respond(t: Task[String], channel: WebSocketChannel): CancelableFuture[Unit] =
+    t.foreach((output) => WebSockets.sendTextBlocking(output, channel))
+
+  override def onConnect(exchange: WebSocketHttpExchange,
+                         channel: WebSocketChannel): Unit = {
+    channel.getReceiveSetter.set(
+      new AbstractReceiveListener() {
+        override def onFullTextMessage(channel: WebSocketChannel,
+                                       message: BufferedTextMessage): Unit = {
+          message.getData match {
+            case ""   => channel.close()
+            case data =>
+              respond(task(data), channel)
+          }
+        }
+      }
+    )
+    channel.resumeReceives()
+  }
 }
