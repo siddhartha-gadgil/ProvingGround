258fc4a644d4fc0546e335b0cc09e33c0c6085a0
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-10-07 18:29:38 +0530

    Tests made for symbolic Nats, closes #193

---
+++ core/src/main/scala/provingground/library/ExampleTheoremsSymbolic.scala
@@ -0,0 +1,93 @@
+package provingground.library
+
+import provingground._
+
+import HoTT._, scalahott._
+
+import induction.TLImplicits._
+
+import shapeless._
+
+import NatRing.{NatTyp => Nt, _}
+
+import spire.implicits._
+
+object SimpleEvensSym {
+  val n = Nt.sym
+
+  val isEven      = "isEven" :: Nt ->: Type
+  val zeroEven    = "0even" :: isEven(Literal(0))
+  val plusTwoEven = "_+2even" :: (n ~>: (isEven(n) ->: isEven(succ(succ(n)))))
+
+
+  val double = n :-> (n + n)
+}
+
+object DoubleEvenSym {
+
+  import SimpleEvensSym._
+
+  val thm = n ~>: isEven(double(n))
+
+  val hyp = "isEven(double(n))" :: isEven(double(n))
+
+  val inductor = NatRing.induc(n :-> isEven(double(n)))
+
+  val pf =
+    inductor(zeroEven) {
+      n :~> (hyp :-> (plusTwoEven(double(n))(hyp)))
+    } !: thm
+}
+
+object SuccNOrNEvenSym {
+  import SimpleEvensSym._
+
+
+  val claim = n :-> (isEven(n) || isEven(succ(n)))
+
+  val base = claim(zero).incl1(zeroEven) !: claim(zero)
+
+  val hyp1 = "n-is-Even" :: isEven(n)
+
+  val hyp2 = "(n+1)-is-Even" :: isEven(succ(n))
+
+  val thm = n ~>: (claim(n))
+
+  val step = n :~> {
+    (claim(n).rec(claim(succ(n)))) {
+      hyp1 :-> (claim(succ(n)).incl2(plusTwoEven(n)(hyp1)))
+    } {
+      hyp2 :-> (claim(succ(n)).incl1((hyp2)))
+    }
+  }
+
+  val inductor = NatRing.induc(claim)
+
+  val pf = inductor(base)(step) !: thm
+}
+
+object LocalConstImpliesConstSym {
+
+  import SimpleEvensSym._
+
+  val A = "A" :: Type
+
+  val f = "f" :: Nt ->: A
+
+  val ass = "assumption" :: n ~>: (f(n) =:= f(succ(n)))
+
+  val claim = n :-> (f(zero) =:= f(n))
+
+  val base = f(zero).refl
+
+  val hyp = "hypothesis" :: (f(zero) =:= f(n))
+  val step = hyp :-> {
+    IdentityTyp.trans(A)(f(zero))(f(n))(f(succ(n)))(hyp)(ass(n))
+  }
+
+  val thm = n ~>: (claim(n))
+
+  val inductor = NatRing.induc(claim)
+
+  val pf = inductor(base)(n :~> step) !: thm
+}
---
+++ mantle/src/test/scala/provingground/InductionSpecSym.scala
@@ -0,0 +1,39 @@
+package provingground
+
+import HoTT._
+import scalahott._
+import org.scalatest.FlatSpec
+import induction.implicits._
+import provingground.library.{DoubleEvenSym, LocalConstImpliesConstSym, SuccNOrNEvenSym}
+// import Fold._
+
+import shapeless._
+import NatRing._
+
+class InductionSpecSym extends FlatSpec {
+
+  val n = NatTyp.sym
+  val m = NatTyp.sym
+
+  val recNN = NatRing.rec(NatTyp)
+  val recNNN = NatRing.rec(NatTyp ->: NatTyp)
+
+  "Ackermann function recursively defined symbolically" should "give the correct values" in {
+    val ackm = "ack(m)" :: NatTyp ->: NatTyp
+
+    val ackmp1n = "ack(m+1)(n)" :: NatTyp
+
+    val ack = recNNN(succ)(
+      m :-> (ackm :-> recNN(ackm(Literal(1)))(n :-> (ackmp1n :-> (ackm(ackmp1n))))))
+
+    assert(ack(Literal(2))(Literal(2)) == Literal(7))
+    assert(ack(Literal(3))(Literal(1)) == Literal(13))
+  }
+
+  "Example Theorems Symbolically proved" should "have proofs with types the theorems" in {
+    assert(DoubleEvenSym.pf.typ == DoubleEvenSym.thm)
+    assert(SuccNOrNEvenSym.pf.typ == SuccNOrNEvenSym.thm)
+    assert(LocalConstImpliesConstSym.pf.typ == LocalConstImpliesConstSym.thm)
+  }
+
+}
