f7f4f1102dd5621cbe085bc63229a5d3b2f68d4c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-11-17 09:01:36 +0530

    experimental entropy difference this is local unlike ratios, so better

--- core/src/main/scala/provingground/learning/EntropyAtomWeight.scala
+++ core/src/main/scala/provingground/learning/EntropyAtomWeight.scala
@@ -4,6 +4,14 @@ import provingground._
 import spire.math._
 import spire.implicits._
 
+/**
+* Spire gradient learning for tradeoff between generation entropy and theorem-proof relative entropy
+  * @param h0 the initial generation entropy
+  * @param kl0 the initial relative entropy between theorems and proofs
+  * @param p0 the initial theorem weight of the element
+  * @param q0 the initial proof weight of the element
+  * @param initWeight the weight of the initial distribution in generation of terms
+  */
 case class EntropyAtomWeight(h0: Double,
                              kl0: Double,
                              p0: Double,
@@ -13,12 +21,12 @@ case class EntropyAtomWeight(h0: Double,
 
   val t: Jet[Double] = Jet.h[Double](0)
 
-  def p1(x: Double): Jet[Double] = 1 / (1 + exp(x + t))
+  def pInit(x: Double): Jet[Double] = 1 / (1 + exp(x + t))
 
-  def q1(x: Double): Jet[Double] = p1(x) * initWeight
+  def q1(x: Double): Jet[Double] = pInit(x) * initWeight
 
   def h1(x: Double): Jet[Double] = {
-    val p = p1(x)
+    val p = pInit(x)
     ((1 - p) * h0) - (p * log(p)) - ((1 - p) * log(1 - p))
   }
 
@@ -29,14 +37,33 @@ case class EntropyAtomWeight(h0: Double,
 
   def rat(x: Double): Jet[Double] = kl1(x) / h1(x)
 
-  def shifted(x: Double, sc: Double = 1): Double =
+  def ratShifted(x: Double, sc: Double = 1): Double =
     x - (rat(x).infinitesimal(0) * sc)
 
-  def iterator(x: Double, sc: Double = 1): Iterator[Double] =
-    Iterator.iterate(x)(y => shifted(y, sc))
+  def ratIterator(x: Double, sc: Double = 1): Iterator[Double] =
+    Iterator.iterate(x)(y => ratShifted(y, sc))
+
+  def diff(x: Double): Jet[Double] = kl1(x) - h1(x)
+
+  def diffShifted(x: Double, sc: Double = 1): Double =
+    x - (diff(x).infinitesimal(0) * sc)
+
+  def diffIterator(x: Double, sc: Double = 1): Iterator[Double] =
+    Iterator.iterate(x)(y => diffShifted(y, sc))
 }
 
 object EntropyAtomWeight {
+  /**
+  * More natural (but verbose) description of spire gradients for entropy tradeoff
+    * @param genDist the initial generating distribution
+    * @param pDist the initial "theorem by proof" distribution
+    * @param qDist the initial "theorem by statement" distribution
+    * @param elem the lemma whose weight is learned
+    * @param initWeight
+    * @tparam A
+    * @tparam B
+    * @return
+    */
   def apply[A, B](genDist: FiniteDistribution[A],
                   pDist: FiniteDistribution[B],
                   qDist: FiniteDistribution[B],
