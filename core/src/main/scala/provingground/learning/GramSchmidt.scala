47c9c08386c225463ca9cc02c7294696364b2735
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-03-15 11:26:21 +0530

    Gram Schmidt and making perpendicular

---
+++ core/src/main/scala/provingground/learning/GramSchmidt.scala
@@ -0,0 +1,30 @@
+package provingground.learning
+
+import spire.algebra._
+import spire.implicits._
+
+object GramSchmidt{
+    def makePerpFromON[V](orthonormals: Vector[V], vec: V)(implicit vs: InnerProductSpace[V, Double]) : V = 
+        orthonormals match {
+            case Vector() => vec
+            case init :+ last =>
+                val recVec = makePerpFromON(init, vec)
+                val minusProj = -1.0 * (last dot recVec)
+                vs.plus(recVec, (minusProj *: last)) 
+        }
+
+    def orthonormal[V](v: Vector[V])(implicit vs: InnerProductSpace[V, Double]) : Vector[V] = 
+        v match {
+            case Vector() => Vector()
+            case init :+ last =>
+                val onInit = orthonormal(init)
+                val perpLast = makePerpFromON(onInit, last)
+                val onLast = perpLast.normalize
+                onInit :+ onLast       
+            }
+
+    def onVec(vv: Vector[Vector[Double]]) = orthonormal(vv)
+
+    def perpVec(vv: Vector[Vector[Double]], v: Vector[Double]) = makePerpFromON(onVec(vv), v)
+
+}
