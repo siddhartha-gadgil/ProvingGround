69e6cce7f7f002f45b8d584842af1efc775dc03e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-04-09 11:38:43 +0530

    abstracted prover to allow sharing with tangential generation

--- core/src/main/scala/provingground/learning/Prover.scala
+++ core/src/main/scala/provingground/learning/Prover.scala
@@ -15,7 +15,8 @@ import EntropyAtomWeight._
 import scalahott.NatRing
 
 case class Prover(
-    initState: TermState = TermState(FiniteDistribution.empty, FiniteDistribution.empty),
+    initState: TermState =
+      TermState(FiniteDistribution.empty, FiniteDistribution.empty),
     tg: TermGenParams = TermGenParams(),
     cutoff: Double = math.pow(10, -4),
     limit: FiniteDuration = 3.minutes,
@@ -25,74 +26,97 @@ case class Prover(
     maxDepth: Int = 10,
     hW: Double = 1,
     klW: Double = 1
-) {
+) extends ProverStep {
   // Convenience for generation
-  def addTerms(terms: (Term, Double)*): Prover =  {
+  def addTerms(terms: (Term, Double)*): Prover = {
     val total = terms.map(_._2).sum
-    val td = initState.terms * (1 - total) ++ FiniteDistribution(terms.map{case (x, p) => Weighted(x, p)})
+    val td = initState.terms * (1 - total) ++ FiniteDistribution(terms.map {
+      case (x, p) => Weighted(x, p)
+    })
     val ts = initState.copy(terms = td)
     this.copy(initState = ts)
   }
 
   lazy val typesByRestrict: Prover = {
-    val typd = initState.terms.collect{case tp: Typ[Term] => tp}.safeNormalized
-    val ts = initState.copy(typs = typd)
+    val typd = initState.terms.collect { case tp: Typ[Term] => tp }.safeNormalized
+    val ts   = initState.copy(typs = typd)
     this.copy(initState = ts)
   }
 
   lazy val typesByMap: Prover = {
     val typd = initState.terms.map(_.typ)
-    val ts = initState.copy(typs = typd)
+    val ts   = initState.copy(typs = typd)
     this.copy(initState = ts)
   }
 
-  def addVars(terms: (Term, Double)*): Prover =  {
+  def addVars(terms: (Term, Double)*): Prover = {
     val total = terms.map(_._2).sum
-    val td = initState.terms * (1 - total) ++ FiniteDistribution(terms.map{case (x, p) => Weighted(x, p)})
+    val td = initState.terms * (1 - total) ++ FiniteDistribution(terms.map {
+      case (x, p) => Weighted(x, p)
+    })
     val allVars = initState.vars ++ terms.map(_._1).toVector
-    val ctx = terms.foldLeft(initState.context){case (cx: Context, (y,_)) => cx.addVariable(y)}
-    val ts = initState.copy(terms = td.safeNormalized, vars = allVars, context = ctx)
+    val ctx = terms.foldLeft(initState.context) {
+      case (cx: Context, (y, _)) => cx.addVariable(y)
+    }
+    val ts =
+      initState.copy(terms = td.safeNormalized, vars = allVars, context = ctx)
     this.copy(initState = ts)
   }
 
-  def addTypes(typs: (Typ[Term], Double)*): Prover =  {
+  def addTypes(typs: (Typ[Term], Double)*): Prover = {
     val total = typs.map(_._2).sum
-    val typd = initState.typs * (1 - total) ++ FiniteDistribution(typs.map{case (x, p) => Weighted(x, p)})
+    val typd = initState.typs * (1 - total) ++ FiniteDistribution(typs.map {
+      case (x, p) => Weighted(x, p)
+    })
     val ts = initState.copy(typs = typd.safeNormalized)
     this.copy(initState = ts)
   }
 
-  def addAxioms(typs: (Typ[Term], Double)*): Prover =  {
+  def addAxioms(typs: (Typ[Term], Double)*): Prover = {
     val total = typs.map(_._2).sum
-    val td = initState.terms * (1 - total) ++ FiniteDistribution(typs.map{case (x, p) => Weighted("axiom":: x, p)})
+    val td = initState.terms * (1 - total) ++ FiniteDistribution(typs.map {
+      case (x, p) => Weighted("axiom" :: x, p)
+    })
     val ts = initState.copy(terms = td.safeNormalized)
     this.copy(initState = ts)
   }
 
-  def addGoals(typs: (Typ[Term], Double)*): Prover =  {
+  def addGoals(typs: (Typ[Term], Double)*): Prover = {
     val total = typs.map(_._2).sum
-    val typd = initState.goals * (1 - total) ++ FiniteDistribution(typs.map{case (x, p) => Weighted(x, p)})
+    val typd = initState.goals * (1 - total) ++ FiniteDistribution(typs.map {
+      case (x, p) => Weighted(x, p)
+    })
     val ts = initState.copy(goals = typd.safeNormalized)
     this.copy(initState = ts)
   }
 
-  def addInd(typ: Typ[Term], intros: Term*)(params: Vector[Term] = Vector(), weight: Double = 1): Prover = {
+  def addInd(
+      typ: Typ[Term],
+      intros: Term*
+  )(params: Vector[Term] = Vector(), weight: Double = 1): Prover = {
     import induction._
     val str0 = ExstInducStrucs.get(typ, intros.toVector)
-    val str = params.foldRight(str0){case (x, s) => ExstInducStrucs.LambdaInduc(x, s)}
-    val dfn = ExstInducDefn(typ, intros.toVector, str)
+    val str = params.foldRight(str0) {
+      case (x, s) => ExstInducStrucs.LambdaInduc(x, s)
+    }
+    val dfn     = ExstInducDefn(typ, intros.toVector, str)
     val indsNew = initState.inds * (1 - weight) ++ FiniteDistribution.unif(dfn)
-    val ts = initState.copy(inds = indsNew)
+    val ts      = initState.copy(inds = indsNew)
     this.copy(initState = ts)
   }
 
-  def addIndexedInd(typ: Term, intros: Term*)(params: Vector[Term] = Vector(), weight: Double = 1): Prover = {
+  def addIndexedInd(
+      typ: Term,
+      intros: Term*
+  )(params: Vector[Term] = Vector(), weight: Double = 1): Prover = {
     import induction._
     val str0 = ExstInducStrucs.getIndexed(typ, intros.toVector)
-    val str = params.foldRight(str0){case (x, s) => ExstInducStrucs.LambdaInduc(x, s)}
-    val dfn = ExstInducDefn(typ, intros.toVector, str)
+    val str = params.foldRight(str0) {
+      case (x, s) => ExstInducStrucs.LambdaInduc(x, s)
+    }
+    val dfn     = ExstInducDefn(typ, intros.toVector, str)
     val indsNew = initState.inds * (1 - weight) ++ FiniteDistribution.unif(dfn)
-    val ts = initState.copy(inds = indsNew)
+    val ts      = initState.copy(inds = indsNew)
     this.copy(initState = ts)
   }
 
@@ -100,36 +124,24 @@ case class Prover(
 
   def isleWeight(w: Double): Prover = this.copy(tg = tg.copy(lmW = w, piW = w))
 
-  def backwardWeight(w: Double): Prover = this.copy(tg = tg.copy(typAsCodW = w, targetInducW = w))
+  def backwardWeight(w: Double): Prover =
+    this.copy(tg = tg.copy(typAsCodW = w, targetInducW = w))
 
-  def negateTypes(w: Double) : Prover = this.copy(tg= tg.copy(negTargetW = w))
+  def negateTypes(w: Double): Prover = this.copy(tg = tg.copy(negTargetW = w))
 
   def natInduction(w: Double = 1.0): Prover = {
-    val mixin =initState.inds + (NatRing.exstInducDefn, w)
+    val mixin = initState.inds + (NatRing.exstInducDefn, w)
     this.copy(initState.copy(inds = mixin.safeNormalized))
   }
 
-  def addSolver(solver: TypSolver): Prover =
-    {
-      val expanded = tg.solver || solver
-      this.copy(tg = tg.copy(solver = expanded))
-    }
+  def addSolver(solver: TypSolver): Prover = {
+    val expanded = tg.solver || solver
+    this.copy(tg = tg.copy(solver = expanded))
+  }
 
   // Proving etc
   val nextState: Task[TermState] =
     tg.nextStateTask(initState, cutoff, limit).memoize
-
-  val evolvedState: Task[EvolvedState] = nextState
-    .map(
-      result => EvolvedState(initState, result, tg, cutoff)
-    )
-    .memoize
-
-  val theoremsByStatement : Task[FiniteDistribution[Typ[Term]]] = nextState.map(_.thmsBySt)
-
-  val theoremsByProof: Task[FiniteDistribution[Typ[Term]]] = nextState.map(_.thmsByPf)
-
-  val unknownStatements: Task[FiniteDistribution[Typ[Term]]] = nextState.map(_.unknownStatements)
 
   val mfd: MonixFiniteDistributionEq[TermState, Term] =
     MonixFiniteDistributionEq(tg.nodeCoeffSeq)
@@ -138,6 +150,52 @@ case class Prover(
     mfd.varDist(initState)(Terms, cutoff)
 
   val equationTerms: Task[Set[EquationTerm]] = pairT.map(_._2).memoize
+
+  // Generating provers using results
+  val withLemmas: Task[Prover] =
+    for {
+      lws <- lemmas
+      lfd = FiniteDistribution(lws.map {
+        case (tp, p) => Weighted("lemma" :: tp, p)
+      })
+      lInit = initState.copy(terms = (initState.terms ++ lfd).safeNormalized)
+    } yield this.copy(initState = lInit)
+
+  val optimalInit: Task[Prover] = expressionEval.map { ev =>
+    val p                            = ev.optimum(hW, klW)
+    val td: FiniteDistribution[Term] = ExpressionEval.dist(Terms, p)
+    val ts                           = initState.copy(terms = td)
+    this.copy(initState = ts)
+  }
+
+}
+
+trait ProverStep {
+  val initState: TermState
+  val nextState: Task[TermState]
+  val tg: TermGenParams
+  val cutoff: Double
+  val scale: Double
+  val steps: Int
+  val maxDepth: Int
+  val limit: FiniteDuration
+
+  val evolvedState: Task[EvolvedState] = nextState
+    .map(
+      result => EvolvedState(initState, result, tg, cutoff)
+    )
+    .memoize
+
+  val theoremsByStatement: Task[FiniteDistribution[Typ[Term]]] =
+    nextState.map(_.thmsBySt)
+
+  val theoremsByProof: Task[FiniteDistribution[Typ[Term]]] =
+    nextState.map(_.thmsByPf)
+
+  val unknownStatements: Task[FiniteDistribution[Typ[Term]]] =
+    nextState.map(_.unknownStatements)
+
+  val equationTerms: Task[Set[EquationTerm]]
 
   val equations: Task[Set[Equation]] = equationTerms.map { eqs =>
     groupEquations(eqs)
@@ -154,15 +212,6 @@ case class Prover(
       ev <- evolvedState
     } yield lemmaWeights(ev, steps, scale)).memoize
 
-  val withLemmas: Task[Prover] =
-    for {
-      lws <- lemmas
-      lfd = FiniteDistribution(lws.map {
-        case (tp, p) => Weighted("lemma" :: tp, p)
-      })
-      lInit = initState.copy(terms = (initState.terms ++ lfd).safeNormalized)
-    } yield this.copy(initState = lInit)
-
   val seek: Task[FiniteDistribution[Term]] =
     for {
       base <- nextState
@@ -178,32 +227,29 @@ case class Prover(
       )
     } yield goals
 
-  val optimalInit: Task[Prover] = expressionEval.map{
-    ev =>
-      val p = ev.optimum(hW, klW)
-      val td: FiniteDistribution[Term] = ExpressionEval.dist(Terms, p)
-      val ts = initState.copy(terms = td)
-      this.copy(initState = ts)
-  }
-
   val functionsForGoals: Task[FiniteDistribution[Term]] =
-    (nextState.flatMap{
-      fs: TermState =>
-        Task.gather(fs.remainingGoals.pmf.map{
+    (nextState
+      .flatMap { fs: TermState =>
+        Task.gather(fs.remainingGoals.pmf.map {
           case Weighted(goal, p) =>
-            val codomainTarget: Task[FiniteDistribution[Term]] =  tg.monixFD.nodeDist(initState)(tg.Gen.codomainNode(goal), cutoff).map(_ * p)
-            val inductionTarget: Task[FiniteDistribution[Term]] =  tg.monixFD.nodeDist(initState)(tg.Gen.targetInducBackNode(goal), cutoff).map(_ * p)
+            val codomainTarget: Task[FiniteDistribution[Term]] = tg.monixFD
+              .nodeDist(initState)(tg.Gen.codomainNode(goal), cutoff)
+              .map(_ * p)
+            val inductionTarget: Task[FiniteDistribution[Term]] = tg.monixFD
+              .nodeDist(initState)(tg.Gen.targetInducBackNode(goal), cutoff)
+              .map(_ * p)
             for {
               dcd <- codomainTarget
               did <- inductionTarget
             } yield (dcd ++ did).safeNormalized
         })
-    }).map(vfd => vfd.foldLeft(FiniteDistribution.empty[Term])(_ ++ _))
+      })
+      .map(vfd => vfd.foldLeft(FiniteDistribution.empty[Term])(_ ++ _))
 
   val subgoals: Task[FiniteDistribution[Typ[Term]]] =
     for {
       funcs <- functionsForGoals
-      fs <- nextState
+      fs    <- nextState
     } yield funcs.flatMap(fs.subGoalsFromFunc _)
 
 }
