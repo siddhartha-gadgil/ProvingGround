84bf32dabca45e328c17cf4b7a1217e0f0b6030c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-06-06 15:17:56 +0530

    better parameters for representation learning

--- mantle/src/main/scala/provingground/learning/RepresentationLearner.scala
+++ mantle/src/main/scala/provingground/learning/RepresentationLearner.scala
@@ -19,6 +19,7 @@ case class IntElem(n: Int) extends SequenceElement() {
 
 class RepresentationLearner[A](
     data: Vector[Vector[A]],
+    vectorLength: Int = 150,
     minWordFrequency: Int = 5,
     batchSize: Int = 250
 ) {
@@ -34,46 +35,52 @@ class RepresentationLearner[A](
   lazy val sequenceIterator =
     new AbstractSequenceIterator.Builder(data.map(seq).asJava).build()
 
-    lazy val vocabCache = new AbstractCache.Builder[IntElem].build()
-
+  lazy val vocabCache = new AbstractCache.Builder[IntElem].build()
 
   lazy val lookupTable = new InMemoryLookupTable.Builder[IntElem]
-    .vectorLength(150)
+    .vectorLength(vectorLength)
     .useAdaGrad(false)
     .cache(vocabCache)
     .build()
 
-  
-  lazy val vectors =
-    {val base = 
-         new SequenceVectors.Builder[IntElem](new VectorsConfiguration())
-      .minWordFrequency(minWordFrequency)
-      .lookupTable(lookupTable)
-      .iterate(sequenceIterator)
-      .vocabCache(vocabCache)
-      .batchSize(batchSize)
-      .iterations(1)
-      .epochs(1)
-      .resetModel(true)
-      .trainElementsRepresentation(true)
-      .trainSequencesRepresentation(false)
-      .elementsLearningAlgorithm(new SkipGram[IntElem]())
-      .build()
-      base.fit()
-      base
-    }
+  lazy val vectors = {
+    val base =
+      new SequenceVectors.Builder[IntElem](new VectorsConfiguration())
+        .minWordFrequency(minWordFrequency)
+        .lookupTable(lookupTable)
+        .iterate(sequenceIterator)
+        .vocabCache(vocabCache)
+        .batchSize(batchSize)
+        .iterations(1)
+        .epochs(1)
+        .resetModel(true)
+        .trainElementsRepresentation(true)
+        .trainSequencesRepresentation(false)
+        .elementsLearningAlgorithm(new SkipGram[IntElem]())
+        .build()
+    base.fit()
+    base
+  }
 
-    lazy val vectorMap : Map[A, Array[Double]] = 
-        elems.map{a => a -> vectors.getWordVector(indexMap(a).toString)}.toMap
+  lazy val vectorMap: Map[A, Array[Double]] =
+    elems.map { a =>
+      a -> vectors.getWordVector(indexMap(a).toString)
+    }.toMap
 
 }
 
-object RepresentationLearner{
-  def fromEquationNodes(eqs: Set[EquationNode], numPaths: Int, length: Int) : RepresentationLearner[GeneratorVariables.Variable[_]] = 
-    {
-      val m = EquationNode.backMap(eqs)
-      val bp = new BackPaths(m)
-      val paths = bp.randomPaths(numPaths, length)
-      new RepresentationLearner(paths)
-    }
+object RepresentationLearner {
+  def equationNodesSampled(
+      eqs: Set[EquationNode],
+      numPaths: Int,
+      length: Int,
+      vectorLength: Int = 150,
+      minWordFrequency: Int = 5,
+      batchSize: Int = 250
+  ): RepresentationLearner[GeneratorVariables.Variable[_]] = {
+    val m     = EquationNode.backMap(eqs)
+    val bp    = new BackPaths(m)
+    val paths = bp.randomPaths(numPaths, length)
+    new RepresentationLearner(paths,vectorLength, minWordFrequency, batchSize)
+  }
 }
