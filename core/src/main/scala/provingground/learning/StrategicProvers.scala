8f190786f9f1afa010ba0f3ca73734848980af7a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-10-21 09:59:44 +0530

    liberal chomping

--- core/src/main/scala/provingground/learning/StrategicProvers.scala
+++ core/src/main/scala/provingground/learning/StrategicProvers.scala
@@ -21,20 +21,23 @@ import collection.mutable.ArrayBuffer
 import _root_.shapeless.Succ
 
 object StrategicProvers {
-  type Successes = Vector[(HoTT.Typ[HoTT.Term], Double, FiniteDistribution[HoTT.Term])]
+  type Successes =
+    Vector[(HoTT.Typ[HoTT.Term], Double, FiniteDistribution[HoTT.Term])]
 
   type SeekResult = (
       Successes,
       Set[EquationNode]
   )
 
-  def formal(sc: Successes) : Set[EquationNode] = {
+  def formal(sc: Successes): Set[EquationNode] = {
     val termsGroups = sc.toSet
-    val terms = termsGroups.map(_._3).flatMap(_.support)
+    val terms       = termsGroups.map(_._3).flatMap(_.support)
     terms.flatMap(t => DE.formalEquations(t))
   }
 
   var currentGoal: Option[Typ[Term]] = None
+
+  var update: Unit => Unit = (_) => ()
 
   def seekGoal(
       lp: LocalProver,
@@ -44,20 +47,34 @@ object StrategicProvers {
   ): Task[SeekResult] = {
     val base = lp.addGoals(typ -> 0.5, negate(typ) -> 0.5)
     currentGoal = Option(typ)
+    update(())
     val tasks = (1 until (maxSteps)).toVector.map { n =>
       val prover = base.sharpen(math.pow(scale, n))
       val pair = for {
         sc  <- prover.successes
         eqs <- prover.equationNodes
-      } yield (sc, eqs union formal(sc) )
+      } yield (sc, eqs union formal(sc))
       pair
     }
 
     bestTask[SeekResult](tasks, p => p._1.nonEmpty)
   }.map(_.getOrElse((Vector(), Set())))
 
-  val successes : ArrayBuffer[Successes] = ArrayBuffer()
+  val successes: ArrayBuffer[Successes] = ArrayBuffer()
 
+  val failures: ArrayBuffer[Typ[Term]] = ArrayBuffer()
+
+  def md =
+    s"""## Goal chomping status
+        |
+        | * current goal : $currentGoal
+        | * successes : ${successes.size}
+        | * failures : ${failures.size}
+        | * last success : ${successes.headOption}
+        | * last failure : ${failures.headOption}
+        |""".stripMargin
+
+  // chomps goals till failure or if none are left
   def goalChomper(
       lp: LocalProver,
       typs: Vector[Typ[Term]],
@@ -69,12 +86,65 @@ object StrategicProvers {
     typs match {
       case Vector() => Task.now((accumSucc, accumEqs, Vector()))
       case typ +: ys =>
-        seekGoal(lp, typ, scale, maxSteps).flatMap { case (ss, eqs) =>
-          if (ss.isEmpty) Task.now((accumSucc :+ ss, accumEqs union eqs, typ +: ys))
-          else
-            {   
-                 successes.append(ss)
-                 goalChomper(lp, ys, accumSucc :+ ss, accumEqs union eqs, scale, maxSteps)
+        seekGoal(lp, typ, scale, maxSteps).flatMap {
+          case (ss, eqs) =>
+            if (ss.isEmpty)
+              Task.now((accumSucc :+ ss, accumEqs union eqs, typ +: ys))
+            else {
+              successes.append(ss)
+              update(())
+              goalChomper(
+                lp,
+                ys,
+                accumSucc :+ ss,
+                accumEqs union eqs,
+                scale,
+                maxSteps
+              )
+            }
+        }
+    }
+
+  def liberalChomper(
+      lp: LocalProver,
+      typs: Vector[Typ[Term]],
+      accumSucc: Vector[Successes] = Vector(),
+      accumFail: Vector[Typ[Term]] = Vector(),
+      accumEqs: Set[EquationNode] = Set(),
+      scale: Double = 2,
+      maxSteps: Int = 100
+  ): Task[
+    (Vector[Successes], Vector[Typ[Term]], Set[EquationNode], Vector[Typ[Term]])
+  ] =
+    typs match {
+      case Vector() => Task.now((accumSucc, accumFail, accumEqs, Vector()))
+      case typ +: ys =>
+        seekGoal(lp, typ, scale, maxSteps).flatMap {
+          case (ss, eqs) =>
+            if (ss.isEmpty) {
+              failures.append(typ)
+              update(())
+              liberalChomper(
+                lp,
+                ys,
+                accumSucc,
+                accumFail :+ typ,
+                accumEqs union eqs,
+                scale,
+                maxSteps
+              )
+            } else {
+              successes.append(ss)
+              update(())
+              liberalChomper(
+                lp,
+                ys,
+                accumSucc :+ ss,
+                accumFail,
+                accumEqs union eqs,
+                scale,
+                maxSteps
+              )
             }
         }
     }
