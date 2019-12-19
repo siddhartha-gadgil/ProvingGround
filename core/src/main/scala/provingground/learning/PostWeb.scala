52b4cd1c9ffbd93ba40229aeecc9de27e1181a02
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-12-19 11:24:15 +0530

    basic post session for autonomy

--- core/src/main/scala/provingground/learning/PostWeb.scala
+++ core/src/main/scala/provingground/learning/PostWeb.scala
@@ -5,6 +5,7 @@ import monix.eval._, monix.tail._
 import monix.execution.Scheduler.Implicits.global
 import shapeless._
 import scala.concurrent.Future
+import scala.collection.mutable.ArrayBuffer
 
 trait Postable[P, W, ID] {
   def post(content: P, web: W, pred: Option[ID]): Task[ID]
@@ -52,11 +53,28 @@ object LocalQueryable {
         stream.headOption.getOrElse(default)
       }
   }
+
+  implicit def hconsQueryable[U, V <: HList, W, ID](
+      implicit qu: LocalQueryable[U, W, ID],
+      qv: LocalQueryable[V, W, ID]
+  ) : LocalQueryable[U :: V, W, ID] = new LocalQueryable[U:: V, W, ID] {
+      def getAt(web: W, id: ID): Task[U :: V] = 
+        for {
+            head <- qu.getAt(web, id)
+            tail <- qv.getAt(web, id)
+        } yield head :: tail
+  }
+
+  implicit def hNilQueryable[W, ID] : LocalQueryable[HNil, W, ID] = new LocalQueryable[HNil, W, ID] {
+      def getAt(web: W, id: ID): Task[HNil] = Task.now(HNil)
+  }
 }
 
 case class PostData[P, W, ID](content: P, id: ID)(
     implicit val pw: Postable[P, W, ID]
-)
+) {
+  def postTo(web: W): Task[ID] = pw.post(content, web, Some(id))
+}
 
 trait PostHistory[W, ID] {
   def history(web: W, id: ID): Stream[PostData[_, W, ID]]
@@ -73,26 +91,26 @@ object PostResponse {
       else None
   }
 
-  def postChainResponseTask[Q, W, ID](
+  def postResponseTask[Q, W, ID](
       web: W,
       post: Q,
       id: ID,
       response: PostResponse[W, ID]
   )(
       implicit qp: Postable[Q, W, ID]
-  ): Task[Set[PostData[_, W, ID]]] = {
+  ): Task[Vector[PostData[_, W, ID]]] = {
     val chainOpt = typedResponseOpt(post, response)(qp)
       .map(tr => tr.postTask(web, post, id))
-      .toSet
+      .toVector
     val flip = Task.sequence(chainOpt)
     flip.map(_.flatten)
   }
 
   def postChainTask[W, ID](
       web: W,
-      posts: Set[PostData[_, W, ID]],
-      responses: Set[PostResponse[W, ID]]
-  ): Task[Set[PostData[_, W, ID]]] = {
+      posts: Vector[PostData[_, W, ID]],
+      responses: Vector[PostResponse[W, ID]]
+  ): Task[Vector[PostData[_, W, ID]]] = {
     val groups =
       for {
         response <- responses
@@ -100,7 +118,7 @@ object PostResponse {
       } yield {
         data match {
           case pd: PostData[a, b, c] =>
-            postChainResponseTask(web, pd.content, pd.id, response)(pd.pw)
+            postResponseTask(web, pd.content, pd.id, response)(pd.pw)
         }
 
       }
@@ -110,14 +128,42 @@ object PostResponse {
 
   def iterant[W, ID](
       web: W,
-      initialPosts: Set[PostData[_, W, ID]],
-      responses: Set[PostResponse[W, ID]]
-  ): Iterant[Task, Set[PostData[_, W, ID]]] = {
-    def step(posts: Set[PostData[_, W, ID]]) =
+      initialPosts: Vector[PostData[_, W, ID]],
+      responses: Vector[PostResponse[W, ID]]
+  ): Iterant[Task, Vector[PostData[_, W, ID]]] = {
+    def step(posts: Vector[PostData[_, W, ID]]) =
       postChainTask(web, posts, responses).map(ps => ps -> ps)
     Iterant.fromLazyStateAction(step)(Task.now(initialPosts))
   }
 
+}
+
+class SimpleSession[W, ID](
+    web: W,
+    id: ID,
+    responses: ArrayBuffer[PostResponse[W, ID]]
+) {
+  def post[P](content: P)(implicit pw: Postable[P, W, ID]): Task[ID] = {
+    val postIdTask = pw.post(content, web, Some(id))
+    postIdTask.foreach { postID =>
+      val reactions = responses.map(
+        response =>
+          PostResponse.postResponseTask(web, content, postID, response).map {
+            v =>
+              v.map {
+                case pd: PostData[q, W, ID] => post(pd.content)(pd.pw)
+              }
+          }
+      )
+      reactions.foreach(_.runToFuture)
+    }
+    postIdTask
+  }
+
+  def query[Q](implicit q: Queryable[Q, W]) = q.get(web)
+
+  def queryAt[Q](id: ID)(implicit q: LocalQueryable[Q, W, ID]) =
+    q.getAt(web, id)
 }
 
 sealed abstract class TypedPostResponse[P, W, ID](
@@ -186,4 +232,73 @@ object TypedPostResponse {
       task
     }
   }
+}
+
+trait GlobalPost[P, ID] {
+  def postGlobal(content: P): Task[ID]
+}
+
+object GlobalPost {
+  class IndexGlobalPost[P] extends GlobalPost[P, Int] {
+    val globalBuffer: ArrayBuffer[P] = ArrayBuffer()
+    def postGlobal(content: P): Task[Int] = {
+      Task {
+        globalBuffer += content
+        val index = globalBuffer.size - 1
+        assert(globalBuffer(index) == content)
+        index
+      }
+    }
+  }
+}
+
+trait PostBuffer[P, ID] extends GlobalPost[P, ID] { self =>
+  val buffer: ArrayBuffer[(P, ID, Option[ID])] = ArrayBuffer()
+
+  def post(content: P, prev: Option[ID]): Task[ID] = {
+    val idT = postGlobal(content)
+    idT.map { id =>
+      buffer += ((content, id, prev))
+      id
+    }
+  }
+
+  def bufferData[W](implicit pw: Postable[P, W, ID]) : Vector[PostData[_, W, ID]] =
+    buffer.map{case (p, id, _) => PostData(p, id)}.toVector
+
+}
+
+object PostBuffer {
+  def get[P, ID](pb: PostBuffer[P, ID], id: ID): Option[P] =
+    pb.buffer.find(_._2 == id).map(_._1)
+
+  def previous[P, ID](pb: PostBuffer[P, ID], id: ID) : Option[ID] =
+    pb.buffer.find(_._2 == id).flatMap(_._3)
+}
+
+class IndexedPostBuffer[P]
+    extends GlobalPost.IndexGlobalPost[P]
+    with PostBuffer[P, Int]
+
+
+object Postable {
+  def apply[P, W, ID](postFunc: (P, W, Option[ID]) => Task[ID]) : Postable[P, W, ID] = 
+    new Postable[P, W, ID] {
+        def post(content: P, web: W, pred: Option[ID]): Task[ID] = 
+            postFunc(content, web, pred)
+    }
+
+  implicit def bufferPostable[P, ID, W <: PostBuffer[P, ID]]
+      : Postable[P, W, ID] =
+    new Postable[P, W, ID] {
+      def post(content: P, web: W, pred: Option[ID]): Task[ID] =
+        web.post(content, pred)
+    }
+
+  implicit def indexedBufferPostable[P, W <: IndexedPostBuffer[P]]
+      : Postable[P, W, Int] =
+    new Postable[P, W, Int] {
+      def post(content: P, web: W, pred: Option[Int]): Task[Int] =
+        web.post(content, pred)
+    }
 }
