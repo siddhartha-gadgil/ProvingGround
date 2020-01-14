e56b648f5b9229f02b1c868dd8ab8661e9d24b2e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-01-14 14:44:40 +0530

    attempted representation Bot

--- mantle/src/main/scala/provingground/learning/RepresentationLearner.scala
+++ mantle/src/main/scala/provingground/learning/RepresentationLearner.scala
@@ -64,9 +64,9 @@ class RepresentationLearner[A](
     base
   }
 
-  lazy val vectorMap: Map[A, Array[Double]] =
+  lazy val vectorMap: Map[A, Vector[Double]] =
     elems.map { a =>
-      a -> vectors.getWordVector(indexMap(a).toString)
+      a -> vectors.getWordVector(indexMap(a).toString).toVector
     }.toMap
 
 }
@@ -119,4 +119,17 @@ object RepresentationLearner {
     val paths = bp.unifRandomPaths(numPaths, length)
     new RepresentationLearner(paths, vectorLength, minWordFrequency, batchSize)
   }
+
+  import TypedPostResponse._
+
+  //   def eqnsToRepLearner(numPaths: Int,
+  //     length: Int,
+  //     coeffWeights: Coeff[_] => Double = (_) => 1,
+  //     backWeight: Double = 0.8,
+  //     vectorLength: Int = 150,
+  //     minWordFrequency: Int = 5,
+  //     batchSize: Int = 250
+  // ) : PostResponse[HoTTPost, HoTTPost.ID] = MicroBot.simple(
+  //   (eqs: Set[EquationNode]) => equationNodesBackSampled(eqs, numPaths, length)
+  // )
 }
