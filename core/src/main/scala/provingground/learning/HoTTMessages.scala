76fb7e03412472d9bae66e93d4700f197da0ee07
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-02-26 13:28:55 +0530

    propogating proofs

--- core/src/main/scala/provingground/learning/HoTTMessages.scala
+++ core/src/main/scala/provingground/learning/HoTTMessages.scala
@@ -9,8 +9,8 @@ import provingground.induction.ExstInducDefn
   *
   * Some messages are instructions for what to do next; some report results
   * and some provide data for use in the future.
-  * 
-  * 
+  *
+  *
   * Some types like `LocalProver`, `ExpressionEval`, `TermGenParams` etc can be posted in their raw form.
   */
 object HoTTMessages {
@@ -55,14 +55,7 @@ object HoTTMessages {
   case class ConsiderInductiveTypes(inducs: FiniteDistribution[ExstInducDefn])
 
   /**
-    * A local prover to use, could be to start things or after tuning.
-    *
-    * @param lp the local prover
-    */
-  case class UseProver(lp: LocalProver)
-
-  /**
-    * modifying parameters, for instance excluding islands, strengthening backward reasoning ertc
+    * modifying parameters, for instance excluding islands, strengthening backward reasoning etc
     *
     * @param modification the modifications
     */
@@ -94,7 +87,7 @@ object HoTTMessages {
     * lemmas that have been identified based on non-triviality (and possibly goals) from a final state.
     *
     * @param lemmas the lemmas with weights
-    * 
+    *
     */
   case class Lemmas(
       lemmas: Vector[(Typ[Term], Double)]
@@ -108,7 +101,7 @@ object HoTTMessages {
     * @param proof proof term
     * @param weight weight
     */
-  case class UseLemma(lemma: Typ[Term], proof: Term, weight: Double)
+  case class UseLemma(lemma: Typ[Term], weight: Double)
 
   /**
     * proceed by tangent evolution, perhaps from a lemma
@@ -153,35 +146,63 @@ object HoTTMessages {
     */
   case class ResolveGoal(goal: Typ[Term]) extends ReasonBackward
 
+  trait Decided {
+    val statement: Typ[Term]
+  }
+
+  case class Proved(statement: Typ[Term], proofOpt: Option[Term])
+      extends Decided {
+    proofOpt.foreach(proof => assert(proof.typ == statement))
+  }
+
+  trait PropogateProof {
+    def propagate(proofs: Set[Term]): Option[Decided]
+  }
+
   case class Consequence(
       premise: Typ[Term],
       conclusion: Typ[Term],
-      proofMap: Term => Term
-  ) {
-    assert(proofMap("hyp" :: premise).typ == conclusion)
+      proofMapOpt: Option[Term => Term]
+  ) extends PropogateProof {
+    def propagate(proofs: Set[Term]): Option[Proved] =
+      proofs.find(_.typ == premise).map { proof =>
+        Proved(conclusion, proofMapOpt.map(m => m(proof)))
+      }
+    assert(
+      proofMapOpt.forall(pfMap => pfMap("hyp" :: premise).typ == conclusion)
+    )
   }
 
   case class Contradicts(
       premise: Typ[Term],
       conclusion: Typ[Term],
-      contraMap: Term => Term => Term
-  ) {
-    assert(contraMap("assume" :: premise)("also" :: conclusion).typ == Zero)
-  }
+      contraMapOpt: Option[Term => Term => Term]
+  ) extends PropogateProof {
+    def propagate(proofs: Set[HoTT.Term]): Option[Decided] =
+      proofs.find(_.typ == premise).map { proof =>
+        val contraOpt =
+          contraMapOpt.map { contraMap =>
+            contraMap(proof)
+          }
+        Contradicted(conclusion, contraOpt)
+      }
 
-  case class Proved(statement: Typ[Term], proof: Term) {
-    assert(proof.typ == statement)
+    contraMapOpt.foreach(
+      contraMap =>
+        assert(contraMap("assume" :: premise)("also" :: conclusion).typ == Zero)
+    )
   }
 
   case class Contradicted(
       statement: Typ[Term],
-      disProof: Term,
-      contra: Term => Term => Term
-  ) {
-    assert(contra(disProof)("assume" :: statement).typ == Zero)
+      contraOpt: Option[Term => Term]
+  ) extends Decided {
+    for {
+      contra <- contraOpt
+    } assert(contra("assume" :: statement).typ == Zero)
   }
 
-  case class RepresenationMap(
+  case class RepresentationMap(
       rep: Map[GeneratorVariables.Variable[_], Vector[Double]]
   )
 
@@ -193,14 +214,29 @@ object HoTTMessages {
 
   case class OptimizeGenerators(damping: Double)
 
+  case class LocalOptimizeGenerators(
+      hW: Double,
+      klW: Double,
+      smoothing: Double,
+      damping: Double
+  )
+
+  case class OptimalInitial(
+      terms: FiniteDistribution[Term],
+      hW: Double,
+      klW: Double,
+      smoothing: Double,
+      damping: Double
+  )
+
   case object GenerateTypes
 
   case object GenerateTerms
 
-  case class SeekInstances[U <: Term with Subs[U]](
+  case class SeekInstances[U <: Term with Subs[U], V <: Term with Subs[V]](
       typ: Typ[U],
-      goal: Typ[Term],
-      proof: U => Typ[Term]
+      val variable: U,
+      goalFamily: Typ[Term]
   )
 
   case class Instance[U <: Term with Subs[U]](term: U, typ: Typ[U]) {
@@ -208,12 +244,39 @@ object HoTTMessages {
   }
 
   case class FromAll(typs: Vector[Typ[Term]], conclusion: Typ[Term])
+      extends PropogateProof {
+    def propagate(proofs: Set[HoTT.Term]): Option[Proved] =
+      if (typs.toSet.subsetOf(proofs.map(_.typ))) Some(Proved(conclusion, None))
+      else None
+  }
 
   case class FromAny(
       typs: Vector[Typ[Term]],
       conclusion: Typ[Term],
       exhaustive: Boolean
-  )
+  ) extends PropogateProof {
+    def propagate(proofs: Set[HoTT.Term]): Option[Decided] =
+      if (typs.toSet.intersect(proofs.map(_.typ)).nonEmpty)
+        Some(Proved(conclusion, None))
+      else if (exhaustive && typs.toSet.subsetOf(proofs.map(_.typ)))
+        Some(Contradicted(conclusion, None))
+      else None
+  }
+
+  @annotation.tailrec
+  def propagateProofs(props: Set[PropogateProof], decisions: Set[Decided]): Set[Decided] = {
+    val proofs = decisions.collect{
+      case Proved(statement, proofOpt) => proofOpt.getOrElse("proved" :: statement)
+    }
+
+    val offspring : Set[Decided] = 
+      for{
+        p <- props
+        pf <- p.propagate(proofs)
+      } yield pf
+
+    if (offspring.nonEmpty) propagateProofs(props, decisions union offspring) else decisions
+  }
 
   case class AddVariable(typ: Typ[Term])
 
