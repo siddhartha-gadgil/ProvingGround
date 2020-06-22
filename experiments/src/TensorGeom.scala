3d25574cb5878c9d2203d9888e185a0522e52f4f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-06-22 14:52:41 +0530

    migration compiles

--- experiments/src/TensorGeom.scala
+++ experiments/src/TensorGeom.scala
@@ -2,30 +2,31 @@ package provingground.experiments
 
 import org.platanios.tensorflow.api._
 
-class TensorGeom(p: Double = 0.5, N: Int = 10) {
-  lazy val q: Output = Tensor(1.0 - p)
+import org.platanios.tensorflow.api.implicits.helpers._
+
+class TensorGeom(p: Float = 0.5.toFloat, N: Int = 10) {
+  lazy val q: Output[Float] = Tensor(1.0.toFloat - p)
 
   lazy val prob = (0 until N).toVector.map { (j) =>
-    tf.variable(s"p$j", FLOAT32, Shape(1, 1), tf.ZerosInitializer)
+    tf.variable[Float](s"p$j", Shape(1, 1), tf.ZerosInitializer)
   }
 
-  lazy val probOut: Vector[Output] = prob.map((x) => x: Output)
+  lazy val probOut: Seq[Output[Any]] = prob.map((x) => x: Output[Float])
 
-  lazy val totErr: Output =
-    tf.square(prob.foldLeft[Output](Tensor(-1.0))(_ + _))
+  lazy val totErr: Output[Float] =
+    tf.square(prob.foldLeft[Output[Float]](Tensor(-1.0.toFloat))(_ + _))
 
   def recEqn(n: Int) = tf.square((q * prob(n - 1)) - prob(n))
 
   lazy val loss = (1 until N).map(recEqn(_)).foldLeft(totErr)(_ + _)
 
-  lazy val trainOp = tf.train.AdaGrad(1.0).minimize(loss)
+  lazy val trainOp = tf.train.AdaGrad(1.0.toFloat).minimize(loss)
 
   lazy val session = Session()
 
   def result = {
-
     session.run(targets = tf.globalVariablesInitializer())
 
-    session.run(fetches = probOut, targets = trainOp)
+    session.run(fetches = probOut.toSeq, targets = trainOp)
   }
 }
