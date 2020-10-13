92adc614028823511b7f0e533612c282938d8bd0
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-10-13 19:51:33 +0530

    avoiding calling Utils from HoTT

--- core/src/main/scala/provingground/HoTT.scala
+++ core/src/main/scala/provingground/HoTT.scala
@@ -579,7 +579,7 @@ object HoTT {
         case pair: AbsPair[u, v] =>
           Some(pair.first)
         case a =>
-          Utils.logger.debug(s"inner evaluation gives $a")
+          // Utils.logger.debug(s"inner evaluation gives $a")
           None
       }
     case RightProjSym(s) =>
@@ -587,7 +587,7 @@ object HoTT {
         case pair: AbsPair[u, v] =>
           Some(pair.second)
         case a =>
-          Utils.logger.debug(s"inner evaluation gives $a")
+          // Utils.logger.debug(s"inner evaluation gives $a")
           None
       }
     case fx: ApplnSym[w, u] =>
