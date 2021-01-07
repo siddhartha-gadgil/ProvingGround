05d50d1a5b6c3b9b069d0a9b5d6741e0ba7762c8
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-01-07 08:28:11 +0530

    compact json including loading, saving, lookup

--- core/src/main/scala/provingground/interface/CompactJson.scala
+++ core/src/main/scala/provingground/interface/CompactJson.scala
@@ -8,6 +8,7 @@ import ujson.Obj
 import ujson.Arr
 import ujson.Num
 import ujson.Bool
+import upickle.implicits.key
 
 object CompactJson {
   type Desc = (Byte, Array[(Byte, Int)])
@@ -17,35 +18,84 @@ object CompactJson {
     Array.fill(l - base.length)(0.toByte) ++ base
   }
 
-  def descToBytes(d: Desc): Array[Byte] =
+  def packDesc(d: Desc): Array[Byte] =
     Array(d._1, d._2.length.toByte) ++ d._2.flatMap {
       case (b, n) => b +: intToBytes(n)
     }
 
-    // head description and the rest
-  def headDesc(bs: Array[Byte]) : (Desc, Array[Byte]) = {
-    val tp = bs(0)
-    val l = bs(1)
+  // head description and the rest
+  def headDesc(bs: Array[Byte]): (Desc, Array[Byte]) = {
+    val tp           = bs(0)
+    val l            = bs(1)
     val (data, tail) = bs.drop(2).splitAt(5 * l)
-    val descData = data.grouped(5).map{
+    val descData = data
+      .grouped(5)
+      .map {
         case gp => (gp.head, BigInt(gp.tail).toInt)
-    }.to(Array)
+      }
+      .to(Array)
     ((tp, descData), tail)
   }
 
   @annotation.tailrec
-  def allDescs(bs: Array[Byte], accum: Array[Desc]): Array[Desc] = 
-    if (bs.isEmpty) accum else {
-        val (head, tail) = headDesc(bs)
-        allDescs(tail, accum :+ head)
+  def unpackDescs(bs: Array[Byte], accum: Array[Desc] = Array()): Array[Desc] =
+    if (bs.isEmpty) accum
+    else {
+      val (head, tail) = headDesc(bs)
+      unpackDescs(tail, accum :+ head)
     }
+
+  def packString(s: String): Array[Byte] = {
+    val bytes = s.getBytes()
+    bytes.size.toByte +: bytes
+  }
+
+  @annotation.tailrec
+  def unpackStrings(
+      bs: Array[Byte],
+      num: Int,
+      accum: Array[String]
+  ): (Array[String], Array[Byte]) =
+    if (num < 1) (accum, bs)
+    else {
+      val l            = bs(0)
+      val (data, tail) = bs.tail.splitAt(l)
+      val s            = new String(data, "UTF-8")
+      unpackStrings(tail, num - 1, accum :+ s)
+    }
+
+  def unpack(bs: Array[Byte]): (Array[Desc], Array[String]) = {
+    val numString           = bs(0)
+    val (strings, descData) = unpackStrings(bs.tail, numString, Array())
+    (unpackDescs(descData), strings)
+  }
+
+  def pack(descs: Array[Desc], keys: Array[String]): Array[Byte] = {
+    val stringPack = keys.length.toByte +: keys.flatMap(packString(_))
+    stringPack ++ descs.flatMap(packDesc(_))
+  }
+
+  def load(
+      bs: Array[Byte],
+      preMap: ujson.Value => ujson.Value = identity,
+      postMap: ujson.Value => ujson.Value = identity
+  ): CompactJson = {
+    val (descs, keys) = unpack(bs)
+    new CompactJson(keys, descs, preMap, postMap)
+  }
+
+  def save(cpt: CompactJson): Array[Byte] =
+    pack(cpt.descriptions.toArray, cpt.keyBuffer.toArray)
+
 }
 
 class CompactJson(
+    initKeys: Iterable[String],
+    initDesc: Iterable[Desc],
     preMap: ujson.Value => ujson.Value = identity,
     postMap: ujson.Value => ujson.Value = identity
 ) {
-  val keyBuffer: ArrayBuffer[String] = ArrayBuffer()
+  val keyBuffer: ArrayBuffer[String] = initKeys.to(ArrayBuffer)
 
   def keyByte(key: String): Byte =
     keyBuffer.zipWithIndex
@@ -57,9 +107,11 @@ class CompactJson(
       }
       .toByte
 
-  var descriptions: ParVector[Desc] = ParVector()
+  var descriptions: ParVector[Desc] = initDesc.to(ParVector)
 
-  var descMap: ParMap[Desc, Int] = ParMap()
+  var descMap: ParMap[Desc, Int] = initDesc.zipWithIndex.to(ParMap)
+
+  def updateMap() = { descMap = descriptions.zipWithIndex.to(ParMap) }
 
   def appendDesc(desc: Desc): Int = {
     descriptions = descriptions :+ desc
@@ -78,9 +130,7 @@ class CompactJson(
   def getDesc(js: ujson.Value, knownNew: Boolean): (Desc, Boolean) =
     preMap(js) match {
       case Str(value) =>
-        val data = value.toCharArray().map(_.toInt).zipWithIndex.map {
-          case (x, n) => (n.toByte, x)
-        }
+        val data = value.getBytes.zipWithIndex
         ((5.toByte, data), false)
       case Obj(value) =>
         val dataFat = value.toArray.map {
@@ -104,7 +154,8 @@ class CompactJson(
         throw new IllegalArgumentException("obtained null json value")
     }
 
-  def getValue(index: Int): ujson.Value = postMap(descValue(descriptions(index)))
+  def getValue(index: Int): ujson.Value =
+    postMap(descValue(descriptions(index)))
 
   def descValue(desc: Desc): ujson.Value = {
     desc match {
@@ -115,7 +166,7 @@ class CompactJson(
       case (4, data) =>
         val kv = data.map { case (k, n) => (keyBuffer(k), getValue(n)) }
         ujson.Obj.from(kv)
-      case (5, data) => data.map(_._2.toChar).mkString("")
+      case (5, data) => new String(data.map(_._1), "UTF-8")
     }
   }
 }
