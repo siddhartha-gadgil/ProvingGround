43aeb20aba1ad275b57991bc64aae169f570d4bc
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-01-08 19:01:50 +0530

    much sharper unapp parallel gen

--- core/src/main/scala/provingground/library/CzSlOly.scala
+++ core/src/main/scala/provingground/library/CzSlOly.scala
@@ -162,7 +162,7 @@ object CzSlOly {
     //   Some(math.pow(10, -11))
     // ),
     parGenUnAppEquations(
-      math.pow(10, -12),
+      math.pow(10, -14),
       12.minutes
     ),
     // cappedForkedTangentEquations,
