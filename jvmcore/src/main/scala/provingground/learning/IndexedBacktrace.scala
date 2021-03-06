3e79e038c7f524448664f0d0c49ee0acc8e129ac
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-03-06 13:26:02 +0530

    correction and documentation for back trace and distances

--- jvmcore/src/main/scala/provingground/learning/IndexedBacktrace.scala
+++ jvmcore/src/main/scala/provingground/learning/IndexedBacktrace.scala
@@ -1,61 +1,120 @@
 package provingground.learning
 
 object IndexedBacktrace {
-  type DistMap = Map[(Int, Int), Double]
 
-  def bestMap(v: Vector[((Int, Int), Double)]): DistMap =
+  /**
+    * known upper bounds on distances, typically only those below a maximum value
+    */
+  type DistBoundMap = Map[(Int, Int), Double]
+
+  /**
+    * map of upper bounds, best from the data
+    *
+    * @param v vector of upper bounds
+    * @return map of best upper bounds
+    */
+  def bestBoundMap(v: Vector[((Int, Int), Double)]): DistBoundMap =
     v.groupMapReduce(_._1)(_._2)(math.min(_, _))
 
-  def symMap(v: Vector[((Int, Int), Double)]): DistMap = {
+  /**
+    * upper bounds refined by using symmetry
+    *
+    * @param v vector of upper bounds
+    * @return symmetric map of upper bounds
+    */
+  def symmetrizedBoundMap(v: Vector[((Int, Int), Double)]): DistBoundMap = {
     val symVec = v ++ v.map { case ((i, j), d) => ((j, i), d) }
-    bestMap(symVec)
+    bestBoundMap(symVec)
   }
 
-  def triangleInequalityStep(
-      m: DistMap
-  ): DistMap = {
+  /**
+    * upper bounds refined using the triangle
+    *
+    * @param m given upper bounds
+    * @return map of deduced upper bounds
+    */
+  def triangleInequalityBounds(
+      m: DistBoundMap
+  ): DistBoundMap = {
     val v = m.toVector
     val transVec = for {
       ((i, j), d1) <- v
       ((l, k), d2) <- v
       if j == l
     } yield ((i, k) -> (d1 + d2))
-    bestMap(v ++ transVec)
+    bestBoundMap(v ++ transVec)
   }
 
+  /**
+    * checks whether a new distance is significantly better than an old one
+    *
+    * @param newDist a possibly improved distance bound
+    * @param oldDist the original distance
+    * @param resolution additive scale for comparison
+    * @return boolean giving whether improvement is significant
+    */
   def distancesAreImproved(
-      newDist: DistMap,
-      oldDist: DistMap,
-      cutoff: Double
+      newDist: DistBoundMap,
+      oldDist: DistBoundMap,
+      resolution: Double
   ): Boolean =
     (newDist.keySet -- oldDist.keySet).nonEmpty ||
-      newDist.exists { case ((i, j), d) => d < oldDist((i, j)) - cutoff }
+      newDist.exists { case ((i, j), d) => d < oldDist((i, j)) - resolution }
 
+  /**
+    * refine distance till there is no significant improvement
+    *
+    * @param baseDistance initial distance
+    * @param refine map giving way to refine
+    * @param resolution additive scale for comparison
+    * @return refined upper bound on distances
+    */
   @annotation.tailrec
   final def stableDistance(
-      baseDistance: DistMap,
-      refine: DistMap => DistMap,
+      baseDistance: DistBoundMap,
+      refine: DistBoundMap => DistBoundMap,
       resolution: Double
-  ): DistMap = {
+  ): DistBoundMap = {
     val newDist = refine(baseDistance)
     if (distancesAreImproved(newDist, baseDistance, resolution))
       stableDistance(newDist, refine, resolution)
     else baseDistance
   }
 
+  /**
+    * refine distances using the triangle inequality with pruning
+    *
+    * @param baseDistance initial distance bounds
+    * @param maxDistance maximum distance bound - larger bounds than these are ignored
+    * @param resolution additive scale for comparison
+    * @return refined upper bound map
+    */
   def triangleStableDistance(
-      baseDistance: DistMap,
+      baseDistance: DistBoundMap,
       maxDistance: Double,
       resolution: Double
-  ) =
+  ): DistBoundMap =
     stableDistance(
       baseDistance,
-      d => triangleInequalityStep(d).filter(_._2 < maxDistance),
+      (d: DistBoundMap) =>
+        triangleInequalityBounds(d).filter(_._2 < maxDistance),
       resolution
     )
 
-  type IndexMatch = (Set[(Int, Int)], Vector[Int], Vector[Int]) // (pairs, umatchedInFirst, unmatchedInSecond)
+  /**
+    * matching of indices for complementary distances,
+    * corresponds to `(pairs, umatchedInFirst, unmatchedInSecond)`
+    */
+  type IndexMatch = (Set[(Int, Int)], Vector[Int], Vector[Int]) //
 
+  /**
+    * all matches of indices satisfying a condition, typically having a good enough distance bount
+    *
+    * @param indices1 first collection of indices
+    * @param indices2 second collection of indices
+    * @param permitted condition for matching
+    * @return vector of index matches
+    */
   def allMatches(
       indices1: Vector[Int],
       indices2: Vector[Int],
@@ -76,20 +135,42 @@ object IndexedBacktrace {
     ) :+ (Set.empty[(Int, Int)], indices1, indices2)).distinct
 }
 
+/**
+  * Computation of probabilities of elements in equations for other elements, and of upper bounds on distances,
+  * based on equations in indices.
+  *
+  * @param rhsExprs right-hand side of equation in terms of indices, with the left hand side being the index.
+  * @param probVec vector of probabilies, i.e., probabilites as function of indices.
+  */
 class IndexedBacktrace(
     rhsExprs: Vector[SumIndexExpression],
     probVec: Vector[Double]
 ) {
   import IndexedBacktrace._
 
-  def numeratorComplement(index: Int, exp: SumIndexExpression): Double =
+  /**
+    * complement of an index in a product term
+    *
+    * @param index a given index
+    * @param exp a product expression
+    * @return Double giving the complementary probability
+    */
+  def numeratorComplement(index: Int, exp: ProductIndexExpression): Double =
     exp.indices
       .filter(_ != index)
       .map { j =>
         probVec(j)
       }
-      .fold(exp.constantTerm)(_ * _)
+      .fold(exp.constant)(_ * _)
 
+  /**
+    * other indices that have significant weight in the given index on tracing back
+    *
+    * @param index initial index
+    * @param cutoff terms with probability below this are ignored
+    * @param headWeight the initial weight, for recursive computation
+    * @return vector of indices with weights
+    */
   def traceBackWeighted(
       index: Int,
       cutoff: Double,
@@ -98,37 +179,63 @@ class IndexedBacktrace(
     if (headWeight < cutoff) Vector()
     else {
       val rhs: SumIndexExpression = rhsExprs(index)
-      val children: Vector[(Int, Double)] = rhs.indices
-        .map(i => i -> (headWeight * probVec(i)))
-        .filter(_._2 > cutoff)
+      val children: Vector[(Int, Double)] =
+        rhs.terms.flatMap(
+          prodTerm =>
+            prodTerm.indices
+              .map(i => i -> (headWeight * numeratorComplement(i, prodTerm)))
+              .filter(_._2 > cutoff)
+        )
       children
         .flatMap {
           case (i, p) =>
-            ((i, headWeight * numeratorComplement(i, rhs))) +: traceBackWeighted(
+            ((i, p)) +: traceBackWeighted(
               i,
               cutoff,
-              headWeight * p
+              p
             )
         }
         .filter(_._2 > cutoff)
     }
 
-  lazy val baseDistances: DistMap = symMap(
+  /**
+    * distance bounds from diagonals and from comparing lhs and rhs of equations
+    *
+    * @return map of distance bounds
+    */
+  lazy val baseDistances: DistBoundMap = symmetrizedBoundMap(
     Vector.tabulate(rhsExprs.size)(i => ((i, i), 0.0)) ++ rhsExprs.zipWithIndex
       .flatMap {
         case (rhs, i) =>
-          rhs.indices.map(
-            j => (i, j) -> (-math.log(numeratorComplement(j, rhs)))
+          rhs.terms.flatMap(
+            prodTerm =>
+              prodTerm.indices.map(
+                j => (i, j) -> (-math.log(numeratorComplement(j, prodTerm)))
+              )
           )
       }
   )
 
+  /**
+    * norms of indices
+    *
+    * @return vector of norms
+    */
   lazy val norms: Vector[Double] = probVec.map(p => -math.log(p))
 
+  /**
+    * distance between product expressions, given by considering all matches where distance bounds are known;
+    * with distance for each match given by distances between matched indices and norms of unmatched ones.
+    *  
+    * @param exp1 first product expression
+    * @param exp2 second product expression
+    * @param baseDistance previous distance bounds
+    * @return distance bound derived by matching
+    */
   def derivedDistance(
-      exp1: SumIndexExpression,
-      exp2: SumIndexExpression,
-      baseDistance: DistMap
+      exp1: ProductIndexExpression,
+      exp2: ProductIndexExpression,
+      baseDistance: DistBoundMap
   ): Double = {
     allMatches(
       exp1.indices,
@@ -142,39 +249,66 @@ class IndexedBacktrace(
     }
   }.min
 
+  /**
+    * Best derived distances between all pairs, filtered by a bound
+    *
+    * @param baseDistance given distance bounds
+    * @param maxDistance maximum distance bound, beyond which bounds are not considered
+    * @return vector of bounds
+    */
   def allDerivedDistances(
-      baseDistance: DistMap,
+      baseDistance: DistBoundMap,
       maxDistance: Double
   ): Vector[((Int, Int), Double)] =
     rhsExprs.zipWithIndex.flatMap {
       case (rhs1, i1) =>
-        (rhsExprs.zipWithIndex
-          .map {
-            case (rhs2, i2) =>
-              val der = derivedDistance(rhs1, rhs2, baseDistance)
-              (i1, i2) -> baseDistance
-                .get((i1, i2))
-                .map(d => math.min(d, der))
-                .getOrElse(der)
-          })
-          .filter(_._2 < maxDistance)
+        rhs1.terms.flatMap(
+          t1 =>
+            (rhsExprs.zipWithIndex
+              .flatMap {
+                case (rhs2, i2) =>
+                  rhs2.terms.map {t2 =>
+                    val der = derivedDistance(t1, t2, baseDistance)
+                    (i1, i2) -> baseDistance
+                      .get((i1, i2))
+                      .map(d => math.min(d, der))
+                      .getOrElse(der)
+                  }
+              })
+              .filter(_._2 < maxDistance)
+        )
     }
 
+  /**
+    * single step of deriving distances and using symmetry and triangle inequality
+    *
+    * @param baseDistance given distance bounds
+    * @param maxDistance maximum distance bound, beyond which bounds are not considered
+    * @param resolution additive scale for comparison
+    * @return refined distance
+    */
   def derivedStep(
-      baseDistance: DistMap,
+      baseDistance: DistBoundMap,
       maxDistance: Double,
       resolution: Double
   ) = {
-    def refine(d: DistMap) =
+    def refine(d: DistBoundMap) =
       triangleStableDistance(
-        symMap(d.toVector ++ allDerivedDistances(d, maxDistance)),
+        symmetrizedBoundMap(d.toVector ++ allDerivedDistances(d, maxDistance)),
         maxDistance,
         resolution
       )
     stableDistance(baseDistance, refine, resolution)
   }
 
-  def finalDistance(maxDistance: Double, resolution: Double): DistMap =
+  /**
+    * stable distance after iterative refinement by deriving and triangle inequality
+    *
+    * @param maxDistance maximum distance bound, beyond which bounds are not considered
+    * @param resolution additive scale for comparison
+    * @return stable refined distance
+    */
+  def finalDistance(maxDistance: Double, resolution: Double): DistBoundMap =
     stableDistance(
       triangleStableDistance(baseDistances, maxDistance, resolution),
       derivedStep(_, maxDistance, resolution),
