f949efc7f9f7dc7740c0728a41ce59bdb3aae995
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-04-23 10:23:49 +0530

    getting vars in layers, tweak to allow batches

--- jvmcore/src/main/scala/provingground/learning/ProofScorePredictor.scala
+++ jvmcore/src/main/scala/provingground/learning/ProofScorePredictor.scala
@@ -41,7 +41,7 @@ object ProofScorePredictor {
   val rnd = new scala.util.Random()
 }
 
-trait ProofScorePredictor {
+trait ProofScorePredictor[ParamVars, TermRepVars, StatRepVars, TopVars] {
   val graph: Graph
   val representationDimension: Int
   val paramLayerDimension: Int
@@ -108,19 +108,21 @@ trait ProofScorePredictor {
       (termProofScoreFan * termProofScore) +
       (termScoreInStatementFan + termScoreInStatement)
 
-  val paramLayers: TFLayers
+  val paramLayers: TFLayers[ParamVars]
 
-  val termRepLayers: TFLayers
+  val termRepLayers: TFLayers[TermRepVars]
 
-  val statementRefLayers: TFLayers
+  val statementRefLayers: TFLayers[StatRepVars]
 
-  val mergedVector = paramLayers.output(paramMergedVector) + termRepLayers
+  val mergedVector = paramLayers.output(
+    tf.reshape(paramMergedVector, tf.constant(Array(paramLayerDimension, 1)))
+  ) + termRepLayers
     .output(termRepresentation) + statementRefLayers.output(
     statementRepresentation
   )
 
   // output except the final sigmoid
-  val preTopLayers: TFLayers
+  val preTopLayers: TFLayers[TopVars]
 
   val prediction = tf.reshape(
     preTopLayers.output(mergedVector),
@@ -145,7 +147,10 @@ trait ProofScorePredictor {
 
   val minimize = optimizer.minimize(loss)
 
-  def runnerWithData(data: TermStatementParameters, session: Session): Session#Runner =
+  def runnerWithData(
+      data: TermStatementParameters,
+      session: Session
+  ): Session#Runner =
     session
       .runner()
       .feed(statementProb, data.statementProbT)
@@ -163,58 +168,94 @@ trait ProofScorePredictor {
 
 }
 
-abstract class TFLayers(implicit tf: Ops) {
+trait TFVars[V] {
+  def getVars(session: Session): V
+}
+abstract class TFLayers[V](implicit tf: Ops) extends TFVars[V] {
+  val inDim: Int
+
+  val outDim: Int
+
   def output(input: Operand[TFloat32]): Operand[TFloat32]
 
-  def |(that: TFLayers) = TFLayers.Composition(this, that)
+  def pipe[X](that: TFLayers[X]) = TFLayers.Composition(this, that)
+  def |[X](that: TFLayers[X])    = pipe(that)
+
+  def stackSigmoid(
+      out: Int
+  )(varInit: Array[Array[Float]] = TFLayers.randomMatix(outDim, out)) =
+    this pipe (new TFLayers.SigmoidLayer(outDim, out)(varInit))
+
 }
 
 object TFLayers {
-  case class Composition(first: TFLayers, second: TFLayers)(implicit tf: Ops)
-      extends TFLayers {
+  def randomMatix(inDim: Int, outDim: Int): Array[Array[Float]] =
+    Array.fill(outDim, inDim)(rnd.nextGaussian().toFloat / inDim.toFloat)
+
+  def getMatrix(inDim: Int, outDim: Int)(data: TFloat32) =
+    Array.tabulate(outDim, inDim) { case (i, j) => data.getFloat(i, j) }
+
+  case class Composition[X, Y](first: TFLayers[X], second: TFLayers[Y])(
+      implicit tf: Ops
+  ) extends TFLayers[(X, Y)] {
+    val inDim: Int = first.inDim
+
+    val outDim: Int = second.outDim
+
+    def getVars(session: Session): (X, Y) =
+      (first.getVars(session), second.getVars(session))
+
     def output(input: Operand[TFloat32]): Operand[TFloat32] =
       first.output(second.output(input))
   }
 
-  class LinearLayer(inDim: Int, outDim: Int)(implicit tf: Ops)
-      extends TFLayers {
-    val matrix = tf.variable(
+  class LinearLayer(val inDim: Int, val outDim: Int)(
+      varInit: Array[Array[Float]] = randomMatix(inDim, outDim)
+  )(implicit tf: Ops)
+      extends TFLayers[Array[Array[Float]]] {
+    val matrix: core.Variable[TFloat32] = tf.variable(
       tf.constant(
-        Array.fill(outDim, inDim)(rnd.nextGaussian().toFloat / inDim.toFloat)
+        varInit
       )
     )
+
+    def getVars(session: Session): Array[Array[Float]] = {
+      val data =
+        session.runner().fetch(matrix).run().get(0).asInstanceOf[TFloat32]
+      getMatrix(inDim, outDim)(data)
+    }
 
     def output(input: Operand[TFloat32]): Operand[TFloat32] =
       tf.reshape(
         tf.linalg
-          .matMul(matrix, tf.reshape(input, tf.constant(Array(inDim, 1)))),
+          .matMul(matrix, input),
         tf.constant(Array(outDim))
       )
   }
 
-  class SigmoidLayer(inDim: Int, outDim: Int)(implicit tf: Ops)
-      extends TFLayers {
-    val matrix = tf.variable(
+  class SigmoidLayer(val inDim: Int, val outDim: Int)(
+      varInit: Array[Array[Float]] = randomMatix(inDim, outDim)
+  )(implicit tf: Ops)
+      extends TFLayers[Array[Array[Float]]] {
+    val matrix: core.Variable[TFloat32] = tf.variable(
       tf.constant(
-        Array.fill(outDim, inDim)(rnd.nextGaussian().toFloat / inDim.toFloat)
+        varInit
       )
     )
+
+    def getVars(session: Session): Array[Array[Float]] = {
+      val data =
+        session.runner().fetch(matrix).run().get(0).asInstanceOf[TFloat32]
+      getMatrix(inDim, outDim)(data)
+    }
 
     def output(input: Operand[TFloat32]): Operand[TFloat32] =
       tf.math.sigmoid(
         tf.reshape(
           tf.linalg
-            .matMul(matrix, tf.reshape(input, tf.constant(Array(inDim, 1)))),
+            .matMul(matrix, input),
           tf.constant(Array(outDim))
         )
       )
-  }
-
-  def sigmoidStack(dims: Int*)(implicit tf: Ops): TFLayers = {
-    val dimVec = dims.toVector
-    val layers = dimVec.zip(dimVec.tail).map {
-      case (inDim, outDim) => new SigmoidLayer(inDim, outDim)
-    }
-    layers.reduce[TFLayers](_ | _)
   }
 }
